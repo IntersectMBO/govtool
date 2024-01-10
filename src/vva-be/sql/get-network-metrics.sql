74728442174559c059672a70a736dcd85e8dd26e
jankun4 <michaljankun@gmail.com>
2024-01-10 17:07:10 +0100

    [57] Update SQL queries for sancho-3-0-0
    `cardano-db-sync` got a new version `sancho-3-0-0` which brings some breaking changes. This commit updates the SQL queries where needed.

--- src/vva-be/sql/get-network-metrics.sql
+++ src/vva-be/sql/get-network-metrics.sql
@@ -10,9 +10,9 @@ with current_epoch as (
 ), total_delegations as (
     select count(*) as count
     from delegation_vote
-), total_governance_actions as (
+), total_gov_action_proposals as (
     select count(distinct(tx_id, index)) as count
-    from governance_action
+    from gov_action_proposal
 ), total_drep_votes as (
     select count(*) as count
     from voting_procedure
@@ -20,6 +20,22 @@ with current_epoch as (
 ), total_registered_dreps as (
     select count(*) as count
     from drep_hash
+), always_abstain_voting_power as (
+    select amount
+    from drep_distr
+    join drep_hash
+    on drep_hash.id = drep_distr.hash_id
+    where drep_hash.view = 'drep_always_abstain'
+    order by epoch_no desc
+    limit 1
+), always_no_confidence_voting_power as (
+    select amount
+    from drep_distr
+    join drep_hash
+    on drep_hash.id = drep_distr.hash_id
+    where drep_hash.view = 'drep_always_no_confidence'
+    order by epoch_no desc
+    limit 1
 )
 
 select
@@ -27,13 +43,17 @@ select
     current_block.block_no,
     unique_delegators.count,
     total_delegations.count,
-    total_governance_actions.count,
+    total_gov_action_proposals.count,
     total_drep_votes.count,
-    total_registered_dreps.count
+    total_registered_dreps.count,
+    always_abstain_voting_power.amount,
+    always_no_confidence_voting_power.amount
 from current_epoch
 cross join current_block
 cross join unique_delegators
 cross join total_delegations
-cross join total_governance_actions
+cross join total_gov_action_proposals
 cross join total_drep_votes
 cross join total_registered_dreps
+cross join always_abstain_voting_power
+cross join always_no_confidence_voting_power

cc57bb055f04d9874ec201d335818fbdf68824da
Adam Guderski <adam@guder.ski>
2024-01-16 14:45:27 +0100

    fix for basic auth exclusion

--- src/scripts/prepare-config.sh
+++ src/scripts/prepare-config.sh
@@ -79,7 +79,7 @@ sed -e "s/GRAFANA_SLACK_RECIPIENT/$GRAFANA_SLACK_RECIPIENT/" \
 # nginx config for frontend optional basic auth
 nginx_config_dir="$target_config_dir/nginx"
 mkdir -p "$nginx_config_dir"
-if [[ "$DOMAIN" == *"govtool.byron.network"* ]]; then
+if [[ "$DOMAIN" == *"sanchonet.govtool.byron.network"* ]]; then
   cat >"$nginx_config_dir/auth.conf" <<_EOF_
 auth_basic "Restricted";
 auth_basic_user_file /etc/nginx/conf.d/govtool.htpasswd;

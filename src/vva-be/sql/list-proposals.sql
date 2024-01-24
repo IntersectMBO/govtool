9fcfeafef7bcc636e953a13891baf1f4091819ef
jankun4 <michaljankun@gmail.com>
2024-01-24 10:46:41 +0100

    [59] Fixed vote counting problem
    Votes were counted in a wrong way, especially when it came to NoConfidence GAs and AlwaysNoConfidence DRep. The SQL query was modified and the problem is fixed

--- src/vva-be/sql/list-proposals.sql
+++ src/vva-be/sql/list-proposals.sql
@@ -9,6 +9,22 @@ WITH LatestDrepDistr AS (
         Max(no) as last_epoch_no,
         Max(end_time) as last_epoch_end_time
     FROM epoch
+), always_no_confidence_voting_power as (
+    select amount
+    from drep_distr
+    join drep_hash
+    on drep_hash.id = drep_distr.hash_id
+    where drep_hash.view = 'drep_always_no_confidence'
+    order by epoch_no desc
+    limit 1
+), always_abstain_voting_power as (
+    select amount
+    from drep_distr
+    join drep_hash
+    on drep_hash.id = drep_distr.hash_id
+    where drep_hash.view = 'drep_always_abstain'
+    order by epoch_no desc
+    limit 1
 )
 
 select
@@ -21,11 +37,22 @@ select
     creator_block.time, /* created date */
     voting_anchor.url,
     encode(voting_anchor.data_hash, 'hex'),
-    coalesce(Sum(ldd.amount) filter (where voting_procedure.vote::text = 'Yes'),0) "yes_votes",
-    coalesce((select no_confidence_drep_distr.amount from drep_distr as no_confidence_drep_distr join drep_hash on no_confidence_drep_distr.hash_id = drep_hash.id where drep_hash.view='drep_always_no_confidence' order by no_confidence_drep_distr.epoch_no desc limit 1) + Sum(ldd.amount) filter (where voting_procedure.vote::text = 'No'), 0) "no_votes",
-    coalesce((select abstain_drep_distr.amount from drep_distr as abstain_drep_distr join drep_hash on abstain_drep_distr.hash_id = drep_hash.id where drep_hash.view='drep_always_abstain' order by abstain_drep_distr.epoch_no desc limit 1), 0) "abstain_votes"
+
+    coalesce(Sum(ldd.amount) filter (where voting_procedure.vote::text = 'Yes'),0)
+        + (case
+                when gov_action_proposal.type = 'NoConfidence' then always_no_confidence_voting_power.amount
+                else 0
+            end) "yes_votes",
+    coalesce(Sum(ldd.amount) filter (where voting_procedure.vote::text = 'No'),0) +
+        (case
+                when gov_action_proposal.type = 'NoConfidence' then 0
+                else always_no_confidence_voting_power.amount
+            end) "no_votes",
+    coalesce(Sum(ldd.amount) filter (where voting_procedure.vote::text = 'Abstain'),0) + always_abstain_voting_power.amount "abstain_votes"
 from gov_action_proposal
 cross join EpochUtils as epoch_utils
+cross join always_no_confidence_voting_power
+cross join always_abstain_voting_power
 join tx as creator_tx
 on creator_tx.id = gov_action_proposal.tx_id
 join block as creator_block
@@ -44,4 +71,16 @@ and gov_action_proposal.enacted_epoch is null
 and gov_action_proposal.expired_epoch is null
 and gov_action_proposal.dropped_epoch is null
 
-group by (gov_action_proposal.id, gov_action_proposal.index, creator_tx.hash, creator_block.time, epoch_utils.epoch_duration, epoch_utils.last_epoch_no, epoch_utils.last_epoch_end_time, voting_anchor.url, voting_anchor.data_hash)
+group by (
+    gov_action_proposal.id,
+    gov_action_proposal.index,
+    creator_tx.hash,
+    creator_block.time,
+    epoch_utils.epoch_duration,
+    epoch_utils.last_epoch_no,
+    epoch_utils.last_epoch_end_time,
+    voting_anchor.url,
+    voting_anchor.data_hash,
+    always_no_confidence_voting_power.amount,
+    always_abstain_voting_power.amount
+    )

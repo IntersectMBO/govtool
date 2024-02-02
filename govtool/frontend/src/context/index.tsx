c0aa804453bc4c8b54c6e024112b3996708f8819
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-02-02 11:32:32 +0100

    delete 2x choose wallet modal render

--- govtool/frontend/src/context/index.tsx
+++ govtool/frontend/src/context/index.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 const ContextProviders = ({ children }: Props) => {
   return (
-    <ModalProvider>
-      <SnackbarProvider>
-        <CardanoProvider>{children}</CardanoProvider>
-      </SnackbarProvider>
-    </ModalProvider>
+    <SnackbarProvider>
+      <CardanoProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </CardanoProvider>
+    </SnackbarProvider>
   );
 };
 

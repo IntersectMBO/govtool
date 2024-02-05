4c05e65069f7768a16532269ab3ffd773a80c9da
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-02-05 13:21:18 +0100

    [#97] Rename S3 bucket for Terraform state in AWS setup
    This commit updates the AWS setup for the GovTool project, particularly focusing on the Terraform configuration. The change involves renaming the S3 bucket used for storing Terraform state, aligning it with the ongoing rebranding effort detailed in ticket #111.
    
    Technical Details:
    - Updated the S3 bucket name in the `bootstrap-aws-account.sh` script within the `infra/terraform` directory. Changed from `vva-terraform-state` to `govtool-terraform-state`.
    - Similarly, modified the Terraform backend configuration in `versions.tf`. The `bucket` field now uses the new `govtool-terraform-state` name.

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vva-terraform-state"
+    bucket         = "govtool-terraform-state"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "vva-terraform-locks"

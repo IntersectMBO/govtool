1b096ebb7e4a3bda8453283a7e4a1eeeebafdeee
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-02-14 17:04:35 +0100

    create FormErrorMessage atom and export from atoms

--- govtool/frontend/src/components/atoms/index.ts
+++ govtool/frontend/src/components/atoms/index.ts
@@ -1,9 +1,12 @@
+export * from "./FormErrorMessage";
 export * from "./ActionRadio";
 export * from "./Background";
 export * from "./Button";
 export * from "./ClickOutside";
 export * from "./CopyButton";
 export * from "./DrawerLink";
+export * from "./HighlightedText";
+export * from "./Input";
 export * from "./Link";
 export * from "./LoadingButton";
 export * from "./modal/Modal";
@@ -13,13 +16,13 @@ export * from "./modal/ModalWrapper";
 export * from "./Radio";
 export * from "./ScrollToManage";
 export * from "./ScrollToTop";
+export * from "./Spacer";
 export * from "./StakeRadio";
 export * from "./Tooltip";
 export * from "./Typography";
 export * from "./VotePill";
 export * from "./VotingPowerChips";
-export * from "./Input";
-export * from "./Spacer";
-export * from "./HighlightedText";
+
+export * from "./checkbox/index";
 
 export * from "./types";

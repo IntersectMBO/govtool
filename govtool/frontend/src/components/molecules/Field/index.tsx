94e37ab1550be1243ba748a9aca16b0ad6cbf566
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-02-14 17:08:15 +0100

    expand field molecule with Checkbox

--- govtool/frontend/src/components/molecules/Field/index.tsx
+++ govtool/frontend/src/components/molecules/Field/index.tsx
@@ -1,15 +1,18 @@
 import React, { PropsWithChildren } from "react";
 
+import { Checkbox } from "./Checkbox";
 import { Input } from "./Input";
 
 type FieldComposition = React.FC<PropsWithChildren> & {
   Input: typeof Input;
+  Checkbox: typeof Checkbox;
 };
 
 const Field: FieldComposition = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>;
 };
 
+Field.Checkbox = Checkbox;
 Field.Input = Input;
 
 export { Field };

b5c838c70e07b822072610b0c58235f20ca5b274
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-02-14 17:03:58 +0100

    create checkbox atom

---
+++ govtool/frontend/src/components/atoms/checkbox/index.tsx
@@ -0,0 +1,30 @@
+import { useId } from "react";
+import { Checkbox as MUICheckbox } from "@mui/material";
+
+import { CheckboxProps } from "../types";
+
+export const Checkbox = ({
+  dataTestId,
+  errorMessage,
+  sx,
+  ...props
+}: CheckboxProps) => {
+  const id = useId();
+
+  return (
+    <MUICheckbox
+      id={id}
+      inputProps={
+        {
+          "data-testid": dataTestId,
+        } as React.InputHTMLAttributes<HTMLInputElement>
+      }
+      sx={{
+        "& .MuiSvgIcon-root": { fontSize: 18 },
+        color: errorMessage ? "red" : "#0033AD",
+        ...sx,
+      }}
+      {...props}
+    />
+  );
+};

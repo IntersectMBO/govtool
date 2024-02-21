5bf9e6d610d1f67c5e43c59700972bf47c47c118
jankun4 <michaljankun@gmail.com>
2024-02-21 17:29:51 +0100

    [#212] provide sole voter information in drep/info
    Extend the drep/info endpoint by adding isRegisteredAsSoleVoter and wasRegisteredAsSoleVoter fields

--- govtool/backend/sql/get-drep-info.sql
+++ govtool/backend/sql/get-drep-info.sql
@@ -1,6 +1,6 @@
 WITH DRepId AS (
     SELECT decode(?, 'hex') as raw
-), IsRegistered AS (
+), IsRegisteredAsDRep AS (
     SELECT (deposit>0) as value,
            deposit as deposit
     FROM drep_registration
@@ -9,9 +9,10 @@ WITH DRepId AS (
     CROSS JOIN DRepId
     WHERE drep_hash.raw = DRepId.raw
     and deposit is not null
+    and drep_registration.voting_anchor_id is not null
     ORDER BY drep_registration.tx_id DESC
     LIMIT 1
-), WasRegistered AS (
+), WasRegisteredAsDRep AS (
     select (EXISTS (
         SELECT *
         FROM drep_registration
@@ -20,9 +21,42 @@ WITH DRepId AS (
         CROSS JOIN DRepId
         WHERE drep_hash.raw = DRepId.raw
         and drep_registration.deposit > 0
+        and drep_registration.voting_anchor_id is not null
+    )) as value
+), IsRegisteredAsSoleVoter AS (
+    SELECT (deposit>0) as value,
+           deposit as deposit
+    FROM drep_registration
+    JOIN drep_hash
+    ON drep_hash.id = drep_registration.drep_hash_id
+    CROSS JOIN DRepId
+    WHERE drep_hash.raw = DRepId.raw
+    and deposit is not null
+    and drep_registration.voting_anchor_id is null
+    ORDER BY drep_registration.tx_id DESC
+    LIMIT 1
+), WasRegisteredAsSoleVoter AS (
+    select (EXISTS (
+        SELECT *
+        FROM drep_registration
+        JOIN drep_hash
+        ON drep_hash.id = drep_registration.drep_hash_id
+        CROSS JOIN DRepId
+        WHERE drep_hash.raw = DRepId.raw
+        and drep_registration.deposit > 0
+        and drep_registration.voting_anchor_id is null
     )) as value
 )
-SELECT IsRegistered.value, WasRegistered.value, IsRegistered.deposit
-FROM WasRegistered
-LEFT JOIN IsRegistered
+SELECT
+    IsRegisteredAsDrep.value,
+    WasRegisteredAsDRep.value,
+    IsRegisteredAsSoleVoter.value,
+    WasRegisteredAsSoleVoter.value,
+    coalesce(IsRegisteredAsDRep.deposit, IsRegisteredAsSoleVoter.deposit)
+FROM WasRegisteredAsDRep
+LEFT JOIN IsRegisteredAsDRep
+ON 1=1
+LEFT JOIN WasRegisteredAsSoleVoter
+ON 1=1
+LEFT JOIN IsRegisteredAsSoleVoter
 ON 1=1

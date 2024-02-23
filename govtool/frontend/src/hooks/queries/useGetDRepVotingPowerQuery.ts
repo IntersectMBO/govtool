a8f51934629ac5885a6ab207692644903e1917bd
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-02-23 11:35:24 +0100

    fix get drep voting power

--- govtool/frontend/src/hooks/queries/useGetDRepVotingPowerQuery.ts
+++ govtool/frontend/src/hooks/queries/useGetDRepVotingPowerQuery.ts
@@ -5,14 +5,14 @@ import { useCardano } from "@context";
 import { getDRepVotingPower } from "@services";
 
 export const useGetDRepVotingPowerQuery = () => {
-  const { dRepID } = useCardano();
+  const { dRepID, dRep } = useCardano();
 
   const { data, isLoading } = useQuery({
     queryKey: QUERY_KEYS.useGetDRepVotingPowerKey,
     queryFn: async () => {
       return await getDRepVotingPower({ dRepID });
     },
-    enabled: !!dRepID,
+    enabled: !!dRepID && dRep?.isRegistered,
   });
 
   return { dRepVotingPower: data, isDRepVotingPowerLoading: isLoading };

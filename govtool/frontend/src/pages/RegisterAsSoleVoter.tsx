4bef8d1e28209155c936f51f3bddf9494cdde926
Michał Szałowski <michal.szalowski@gmail.com>
2024-02-27 10:54:10 +0100

    [#119] Post code review adjustments

--- govtool/frontend/src/pages/RegisterAsSoleVoter.tsx
+++ govtool/frontend/src/pages/RegisterAsSoleVoter.tsx
@@ -4,18 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { PATHS } from "@consts";
 import { RegisterAsSoleVoterBox } from "@organisms";
 import { useTranslation } from "@hooks";
-import { WALLET_LS_KEY, getItemFromLocalStorage } from "@/utils/localStorage";
 import { CenteredBoxPageWrapper } from "@molecules";
+import { checkIsWalletConnected } from "@/utils";
 
 export const RegisterAsSoleVoter = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
 
   useEffect(() => {
-    if (
-      !getItemFromLocalStorage(`${WALLET_LS_KEY}_stake_key`) ||
-      !getItemFromLocalStorage(`${WALLET_LS_KEY}_name`)
-    ) {
+    if (checkIsWalletConnected()) {
       navigate(PATHS.home);
     }
   }, []);

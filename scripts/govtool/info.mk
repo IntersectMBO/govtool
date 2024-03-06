988858a395f4442e3565d1897d451aeead7e7861
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-06 09:13:37 +0100

    [#361] Implement deployment traceability and streamline notification process
    - Modified 'all' target in Makefile to exclude 'info' and directly
      include 'notify', simplifying the deployment sequence.
    - Introduced 'log-deployment' in info.mk for logging deployment details
      to /var/log/deployment.log. This includes capturing essential
      deployment metadata such as the environment, branch, commit, and
      versions of the deployed components, formatted with a timestamp and
      marked as INFO for clarity.
    - Updated 'notify' target to depend on 'info' and 'log-deployment',
      ensuring comprehensive deployment information is shared and recorded.
      This includes deployment success notifications with detailed component
      versions and source information.
    - Adjusted the format for logging deployment information to improve
      readability and consistency in monitoring tools.
    
    These changes enhance the project's deployment traceability, allowing
    for better tracking of deployments and easier debugging and auditing of
    deployed versions.

--- scripts/govtool/info.mk
+++ scripts/govtool/info.mk
@@ -1,10 +1,18 @@
 # pipeline information
 pipeline_url := $(shell echo $${PIPELINE_URL:-})
-ifeq ($(PIPELINE_URL),)
+ifeq ($(pipeline_url),)
   pipeline_info := _Deployed from local machine._
 else
   pipeline_info := _Deployed from <$(pipeline_url)|GitHub>._
 endif
+
+deployment_log_file := /var/log/deployment.log
+
+.PHONY: log-deployment
+log-deployment:
+	@:$(call check_defined, cardano_network)
+	@:$(call check_defined, env)
+	ssh $(ssh_url) 'echo "$(shell date +'%Y-%m-%d %H:%M:%S%z') INFO: $(branch) ($(commit)); cardano-node $(cardano_node_image_tag); cardano-dbsync $(cardano_db_sync_image_tag); backend $(backend_image_tag); frontend $(frontend_image_tag); $(pipeline_url)" >> $(deployment_log_file)'
 
 .PHONY: info
 info:
@@ -24,11 +32,10 @@ info:
 	@echo "+-----------"
 
 .PHONY: notify
-notify:
+notify: info log-deployment
 	@:$(call check_defined, cardano_network)
 	@:$(call check_defined, env)
 	$(curl) -X POST https://slack.com/api/chat.postMessage\
 		-H "Authorization: Bearer $${GRAFANA_SLACK_OAUTH_TOKEN}" \
 		-H "Content-Type: application/json; charset=utf-8" \
 		--data "{ \"channel\":\"$${GRAFANA_SLACK_RECIPIENT}\", \"text\":\":rocket: *Deploy performed on \`$(env)\`*\n- from *branch* \`$(branch)\` (\`$(commit)\`),\n- using *Cardano Node* version \`$(cardano_node_image_tag)\`,\n- using *Cardano DB Sync* version \`$(cardano_db_sync_image_tag)\`,\n- using *GovTool backend* version \`$(backend_image_tag)\`,\n- using *Govtool frontend* version \`$(frontend_image_tag)\`.\n$(pipeline_info)\" }"
-

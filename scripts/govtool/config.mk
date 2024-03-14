3edc22fe0d312cc06b8b791eb981dab507ea8e82
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-14 08:20:28 +0100

    [#436] Refactor usage of `directories` variable for improved structuring
    This refactor aims to enhance the deployment makefiles by optimizing the
    `directories` variable usage for improved directory structuring. By
    ensuring better utilization of this variable, the file targets will be
    more accurately defined, allowing for a more organized and controlled
    configuration generation process.
    
    Changes made:
    - Updated the `config.mk` file in the `scripts/govtool` directory to
      reorganize the `directories` variable usage.
    - Introduced a new `cardano_config_provider` metadata variable for
      clarity and separation of concerns.
    - Restructured the variable assignments for `config_dir`,
      `template_config_dir`, `target_config_dir`, and `config_subdirs`.
    - Modified the file and directory definitions resulting in a more
      coherent structure and improved maintainability.
    - Removed duplicate assignments and enhanced directory creation commands
      using the `output_dirs` variable to streamline the process.
    - Adjusted dependencies and file generation rules to align with the
      updated directory structure, ensuring accurate target locations for
      file outputs.
    
    This refactor optimizes the directory variable usage in the `config.mk`
    file, improving the organization and maintainability of the deployment
    configuration generation process.

--- scripts/govtool/config.mk
+++ scripts/govtool/config.mk
@@ -4,22 +4,22 @@ ifeq ($(origin $(common_mk)), undefined)
   include $(common_mk)
 endif
 
-# directory paths
+# metadata
+cardano_config_provider := https://book.world.dev.cardano.org
+
+# directories
 config_dir := $(root_dir)/scripts/govtool/config
 template_config_dir := $(config_dir)/templates
 target_config_dir := $(config_dir)/target
-config_subdirs := cardano-node dbsync-secrets grafana-provisioning nginx grafana-provisioning/datasources grafana-provisioning/dashboards grafana-provisioning/alerting
-target_config_subdirs := $(target_config_dir) $(addprefix $(target_config_dir)/,$(config_subdirs))
-
-# metadata
-cardano_config_provider := https://book.world.dev.cardano.org
 
 # files
 docker_compose_file := $(target_config_dir)/docker-compose.yml
 cardano_configs := alonzo-genesis byron-genesis conway-genesis db-sync-config shelley-genesis submit-api-config topology
 cardano_config_files := $(addprefix $(target_config_dir)/cardano-node/,$(addsuffix .json,$(cardano_configs)))
 outputs := cardano-node/config.json dbsync-secrets/postgres_user dbsync-secrets/postgres_db dbsync-secrets/postgres_password backend-config.json prometheus.yml promtail.yml loki.yml nginx/auth.conf nginx/govtool.htpasswd grafana-provisioning/alerting/alerting.yml grafana-provisioning/datasources/datasource.yml grafana-provisioning/dashboards/dashboard.yml grafana-provisioning/dashboards/govtool.json  grafana-provisioning/dashboards/traefik_rev4.json
+
 output_files := $(addprefix $(target_config_dir)/,$(outputs)) $(cardano_config_files) $(docker_compose_file)
+output_dirs := $(sort $(foreach file,$(output_files),$(dir $(file))))
 
 .PHONY: prepare-config
 prepare-config: clear $(output_files)
@@ -29,10 +29,10 @@ clear:
 	rm -rf $(target_config_dir)
 
 .SECONDEXPANSION:
-$(target_config_subdirs):
+$(output_dirs):
 	mkdir -p $@
 
-$(target_config_dir)/docker-compose.yml: $(template_config_dir)/docker-compose.yml.tpl $(target_config_dir)
+$(target_config_dir)/docker-compose.yml: $(template_config_dir)/docker-compose.yml.tpl $(target_config_dir)/
 	if [[ "$(env)" == "dev" ]]; then CSP_ALLOWED_HOSTS=",http://localhost"; else CSP_ALLOWED_HOSTS=; fi; \
 	sed -e "s|<DOMAIN>|$(domain)|g" \
 		-e "s|<DOCKER_USER>|$(docker_user)|g" \
@@ -40,47 +40,47 @@ $(target_config_dir)/docker-compose.yml: $(template_config_dir)/docker-compose.y
 		-e "s|<CSP_ALLOWED_HOSTS>|$${CSP_ALLOWED_HOSTS}|g" \
 		$< > $@
 
-$(cardano_config_files): $(target_config_dir)/cardano-node
+$(cardano_config_files): $(target_config_dir)/cardano-node/
 	@:$(call check_defined, cardano_network)
 	$(curl) -s "$(cardano_config_provider)/environments/$(cardano_network)/$(notdir $@)" -o $@
 
 .PHONY: $(target_config_dir)/cardano-node/config.json
-$(target_config_dir)/cardano-node/config.json: $(target_config_dir)/cardano-node
+$(target_config_dir)/cardano-node/config.json: $(target_config_dir)/cardano-node/
 	$(curl) -s "$(cardano_config_provider)/environments/$(cardano_network)/$(notdir $@)" -o $@
 	sed -i '/"hasPrometheus"/ { N; s/"127\.0\.0\.1"/"0.0.0.0"/ }' "$(target_config_dir)/cardano-node/config.json"
 
-$(target_config_dir)/dbsync-secrets/postgres_user: $(target_config_dir)/dbsync-secrets
+$(target_config_dir)/dbsync-secrets/postgres_user: $(target_config_dir)/dbsync-secrets/
 	echo "$${DBSYNC_POSTGRES_USER}" > $@
 
-$(target_config_dir)/dbsync-secrets/postgres_password: $(target_config_dir)/dbsync-secrets
+$(target_config_dir)/dbsync-secrets/postgres_password: $(target_config_dir)/dbsync-secrets/
 	echo "$${DBSYNC_POSTGRES_PASSWORD}" > $@
 
-$(target_config_dir)/dbsync-secrets/postgres_db: $(target_config_dir)/dbsync-secrets
+$(target_config_dir)/dbsync-secrets/postgres_db: $(target_config_dir)/dbsync-secrets/
 	echo "$${DBSYNC_POSTGRES_DB}" > $@
 
-$(target_config_dir)/backend-config.json: $(config_dir)/templates/backend-config.json.tpl $(target_config_dir)
+$(target_config_dir)/backend-config.json: $(config_dir)/templates/backend-config.json.tpl $(target_config_dir)/
 	sed -e "s|<DBSYNC_POSTGRES_DB>|$${DBSYNC_POSTGRES_DB}|" \
 		-e "s|<DBSYNC_POSTGRES_USER>|$${DBSYNC_POSTGRES_USER}|" \
 		-e "s|<DBSYNC_POSTGRES_PASSWORD>|$${DBSYNC_POSTGRES_PASSWORD}|" \
 		-e "s|<SENTRY_DSN>|$${SENTRY_DSN_BACKEND}|" \
 		$< > $@
 
-$(target_config_dir)/%.yml: $(template_config_dir)/%.yml $(target_config_dir)
+$(target_config_dir)/%.yml: $(template_config_dir)/%.yml $(target_config_dir)/
 	cp -a $< $@
 
-$(target_config_dir)/grafana-provisioning/datasources/%: $(template_config_dir)/grafana-provisioning/datasources/% $(target_config_dir)/grafana-provisioning/datasources
+$(target_config_dir)/grafana-provisioning/datasources/%: $(template_config_dir)/grafana-provisioning/datasources/% $(target_config_dir)/grafana-provisioning/datasources/
 	cp -a $< $@
 
-$(target_config_dir)/grafana-provisioning/dashboards/%: $(template_config_dir)/grafana-provisioning/dashboards/% $(target_config_dir)/grafana-provisioning/dashboards
+$(target_config_dir)/grafana-provisioning/dashboards/%: $(template_config_dir)/grafana-provisioning/dashboards/% $(target_config_dir)/grafana-provisioning/dashboards/
 	cp -a $< $@
 
-$(target_config_dir)/grafana-provisioning/alerting/alerting.yml: $(template_config_dir)/grafana-provisioning/alerting/alerting.yml $(target_config_dir)/grafana-provisioning/alerting
+$(target_config_dir)/grafana-provisioning/alerting/alerting.yml: $(template_config_dir)/grafana-provisioning/alerting/alerting.yml $(target_config_dir)/grafana-provisioning/alerting/
 	cp -a $< $@
 	sed -e "s|<GRAFANA_SLACK_RECIPIENT>|$${GRAFANA_SLACK_RECIPIENT}|" \
 		-e "s|<GRAFANA_SLACK_OAUTH_TOKEN>|$${GRAFANA_SLACK_OAUTH_TOKEN}|" \
 		-i $@
 
-$(target_config_dir)/nginx/auth.conf: $(target_config_dir)/nginx
+$(target_config_dir)/nginx/auth.conf: $(target_config_dir)/nginx/
 	@:$(call check_defined, domain)
 	if [[ "$(domain)" == *"sanchonet.govtool.byron.network"* ]]; then \
 	  echo "auth_basic \"Restricted\";" > $@; \
@@ -89,7 +89,7 @@ $(target_config_dir)/nginx/auth.conf: $(target_config_dir)/nginx
 	  echo > $@; \
 	fi
 
-$(target_config_dir)/nginx/govtool.htpasswd: $(target_config_dir)/nginx
+$(target_config_dir)/nginx/govtool.htpasswd: $(target_config_dir)/nginx/
 	@:$(call check_defined, domain)
 	if [[ "$(domain)" == *"sanchonet.govtool.byron.network"* ]]; then \
 	  echo "$${NGINX_BASIC_AUTH}" > $@; \

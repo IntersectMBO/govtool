6ac45d4ab69f5dd189e1d1905451b51aceedc317
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-14 07:57:01 +0100

    [#436] Refactor grafana provisioning target
    Refactoring the Grafana provisioning target to enhance the deployment
    makefiles by replacing phony targets with actual file targets. This
    change improves the file-driven configuration generation process and
    allows for more declarative control over the configuration setup.
    
    Changes:
    - Modified the `config.mk` file within `scripts/govtool` directory.
    - Updated `config_subdirs` list to include additional Grafana
      provisioning subdirectories: datasources, dashboards, and alerting.
    - Updated `outputs` list to include files related to the newly added
      Grafana provisioning subdirectories.
    - Removed the `prepare-grafana-provisioning` as a phony target and
      directly integrated the actions within the respective file targets for
      datasources, dashboards, and alerting.
    - Added individual file targets for copying and processing datasources,
      dashboards, and the alerting configuration YAML files into the Grafana
      provisioning directory, ensuring customization capabilities.
    - Adjusted the sed command to modify the Grafana alerting configuration
      file path to the correct target directory path.
    
    This commit streamlines the Grafana provisioning process within the
    deployment makefiles, improving clarity, maintainability, and
    configurability of the deployment configurations.

--- scripts/govtool/config.mk
+++ scripts/govtool/config.mk
@@ -8,7 +8,7 @@ endif
 config_dir := $(root_dir)/scripts/govtool/config
 template_config_dir := $(config_dir)/templates
 target_config_dir := $(config_dir)/target
-config_subdirs := cardano-node dbsync-secrets grafana-provisioning nginx
+config_subdirs := cardano-node dbsync-secrets grafana-provisioning nginx grafana-provisioning/datasources grafana-provisioning/dashboards grafana-provisioning/alerting
 target_config_subdirs := $(target_config_dir) $(addprefix $(target_config_dir)/,$(config_subdirs))
 
 # metadata
@@ -18,11 +18,11 @@ cardano_config_provider := https://book.world.dev.cardano.org
 docker_compose_file := $(target_config_dir)/docker-compose.yml
 cardano_configs := alonzo-genesis byron-genesis conway-genesis db-sync-config shelley-genesis submit-api-config topology
 cardano_config_files := $(addprefix $(target_config_dir)/cardano-node/,$(addsuffix .json,$(cardano_configs)))
-outputs := cardano-node/config.json dbsync-secrets/postgres_user dbsync-secrets/postgres_db dbsync-secrets/postgres_password backend-config.json prometheus.yml promtail.yml loki.yml nginx/auth.conf nginx/govtool.htpasswd
+outputs := cardano-node/config.json dbsync-secrets/postgres_user dbsync-secrets/postgres_db dbsync-secrets/postgres_password backend-config.json prometheus.yml promtail.yml loki.yml nginx/auth.conf nginx/govtool.htpasswd grafana-provisioning/alerting/alerting.yml grafana-provisioning/datasources/datasource.yml grafana-provisioning/dashboards/dashboard.yml grafana-provisioning/dashboards/govtool.json  grafana-provisioning/dashboards/traefik_rev4.json
 output_files := $(addprefix $(target_config_dir)/,$(outputs)) $(cardano_config_files) $(docker_compose_file)
 
 .PHONY: prepare-config
-prepare-config: clear $(output_files) prepare-grafana-provisioning
+prepare-config: clear $(output_files)
 
 .PHONY: clear
 clear:
@@ -68,12 +68,17 @@ $(target_config_dir)/backend-config.json: $(config_dir)/templates/backend-config
 $(target_config_dir)/%.yml: $(template_config_dir)/%.yml $(target_config_dir)
 	cp -a $< $@
 
-.PHONY: prepare-grafana-provisioning
-prepare-grafana-provisioning: $(target_config_dir)/grafana-provisioning
-	cp -a $(template_config_dir)/grafana-provisioning/* $(target_config_dir)/grafana-provisioning
+$(target_config_dir)/grafana-provisioning/datasources/%: $(template_config_dir)/grafana-provisioning/datasources/% $(target_config_dir)/grafana-provisioning/datasources
+	cp -a $< $@
+
+$(target_config_dir)/grafana-provisioning/dashboards/%: $(template_config_dir)/grafana-provisioning/dashboards/% $(target_config_dir)/grafana-provisioning/dashboards
+	cp -a $< $@
+
+$(target_config_dir)/grafana-provisioning/alerting/alerting.yml: $(template_config_dir)/grafana-provisioning/alerting/alerting.yml $(target_config_dir)/grafana-provisioning/alerting
+	cp -a $< $@
 	sed -e "s|<GRAFANA_SLACK_RECIPIENT>|$${GRAFANA_SLACK_RECIPIENT}|" \
 		-e "s|<GRAFANA_SLACK_OAUTH_TOKEN>|$${GRAFANA_SLACK_OAUTH_TOKEN}|" \
-		-i $(target_config_dir)/grafana-provisioning/alerting/alerting.yml
+		-i $@
 
 $(target_config_dir)/nginx/auth.conf: $(target_config_dir)/nginx
 	@:$(call check_defined, domain)

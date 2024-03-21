d73d7bc3cc7e24c370cfc1e20fed7c731f3ea896
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-21 09:43:34 +0100

    [#526] Add a default entrypoint for scripts
    Implementing Nix Flakes requires the creation of a default entrypoint
    for scripts to ensure that that application module can be effectively
    combined within the project structure. This default.nix file includes a
    shell definition with necessary build inputs such as awscli, curl,
    docker, git, gnumake, openssh, and rsync. By adding this default
    entrypoint, developers can utilize Nix Flakes to streamline the
    combination of application modules within the project.

---
+++ scripts/govtool/default.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [ awscli curl docker git gnumake openssh rsync ];
+  shellHook = ''
+    function warn() {
+      tput setaf $2; echo "$1"; tput sgr0
+    }
+
+    tput bold
+    warn "Welcome to GovTool!" 4
+    warn "This is a deployment shell." 4
+    warn "Read the scripts/govtool/README.md to get more info about the deployment processes." 8
+    case "''${ENVIRONMENT}" in
+      "dev")
+        export DOMAIN="dev-''${CARDANO_NETWORK}.govtool.byron.network"
+        warn "Your configuration is set to deploy to DEV environment, you are safe." 2
+        ;;
+      "test")
+        export DOMAIN="test-''${CARDANO_NETWORK}.govtool.byron.network"
+        warn "Your configuration is set to deploy to TEST environment, be careful…" 5
+        ;;
+      "staging")
+        export DOMAIN="staging.govtool.byron.network"
+        warn "Your configuration is set to deploy to STAGING environment, be careful…" 5
+        ;;
+      "beta")
+        export DOMAIN="sanchogov.tools"
+        warn "Your configuration is set to deploy to BETA environment, BE CAREFUL!" 1
+        ;;
+      *)
+        warn "Your configuration is not set, deployment will not work…" 1
+        ;;
+    esac
+  '';
+}

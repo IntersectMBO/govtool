1d4a95447763b26aa0876f997b02090a6c623db0
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-21 12:34:53 +0100

    [#526] Incorporate backend environment into Nix Flakes as an output
    The user story necessitates utilizing Nix Flakes to combine application
    modules within the project for better organization and manageability.
    This commit integrates the backend environment into the Nix Flakes as
    one of the outputs, aligning with the objective of enhancing
    accessibility and usability of application modules from the project's
    root. By adding the backend as an output, developers can effectively
    combine and utilize the backend environment using Nix Flakes for
    improved project structure and management.
    
    Changes:
    - Added the backend environment as an output in the `flake.nix` file.
    - Modified `govtool/backend/default.nix` to include the recommended
      Haskell compiler version and additional tools needed for the backend.
    - Updated the entrypoint to provide necessary build inputs and display a
      welcome message for the backend development shell.
    - Removed unnecessary files `shell.nix` and `sources.nix` related to the
      backend environment setup.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "GovTool and utilities monorepo.";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/c9ece0059f42e0ab53ac870104ca4049df41b133";
+  inputs.nixpkgs.url =
+    "github:nixos/nixpkgs/c9ece0059f42e0ab53ac870104ca4049df41b133";
 
   outputs = { self, nixpkgs }: {
     packages = let
@@ -12,11 +13,13 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ ];
+          config.allowBroken = true;
         };
       in {
         name = system;
         value = {
           scripts = pkgs.callPackage ./scripts/govtool { inherit pkgs; };
+          backend = pkgs.callPackage ./govtool/backend { inherit pkgs; };
         };
       }) supportedSystems);
   };
--- govtool/backend/sources.nix
+++
@@ -1,8 +0,0 @@
-{
-  pkgs = import
-    (builtins.fetchGit {
-      url = "https://github.com/NixOS/nixpkgs.git";
-      rev = "c9ece0059f42e0ab53ac870104ca4049df41b133";
-    })
-    { };
-}

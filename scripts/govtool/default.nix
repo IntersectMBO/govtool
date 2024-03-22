686b62c91f5800c402c95819eff08668ad571fb1
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-22 08:33:09 +0100

    [#526] Move DOMAIN definition to the master .envrc file in the root of the project
    The relocation of the DOMAIN definition from the subdirectory specific
    .envrc files to the master .envrc file in the root directory streamlines
    the access to and management of environment variables within the
    project. As per the user story requirements to enhance organization and
    accessibility of application modules using Nix Flakes, this adjustment
    ensures that the domain configuration is centralized, simplifying the
    integration of the application modules. By consolidating this setting in
    the main .envrc file, developers can effectively coordinate and utilize
    the domain configuration across different project components, aligning
    with the goal of leveraging Nix Flakes for improved project structure
    and ease of use.
    
    Changes:
    - Moved the DOMAIN definition logic from individual .envrc files to the
      main .envrc file in the project's root directory.
    - Updated the .envrc.example file to reflect the changes in domain
      configuration logic for different environments.
    - Streamlined domain configuration setups for various environments (dev,
      test, staging, beta) within the central .envrc file, enhancing clarity
      and accessibility for developers working on the project.

--- scripts/govtool/default.nix
+++ scripts/govtool/default.nix
@@ -10,19 +10,15 @@ pkgs.mkShell {
     warn "Read the scripts/govtool/README.md to get more info about the deployment processes." 8
     case "''${ENVIRONMENT}" in
       "dev")
-        export DOMAIN="dev-''${CARDANO_NETWORK}.govtool.byron.network"
         warn "Your configuration is set to deploy to DEV environment, you are safe." 2
         ;;
       "test")
-        export DOMAIN="test-''${CARDANO_NETWORK}.govtool.byron.network"
         warn "Your configuration is set to deploy to TEST environment, be careful…" 5
         ;;
       "staging")
-        export DOMAIN="staging.govtool.byron.network"
         warn "Your configuration is set to deploy to STAGING environment, be careful…" 5
         ;;
       "beta")
-        export DOMAIN="sanchogov.tools"
         warn "Your configuration is set to deploy to BETA environment, BE CAREFUL!" 1
         ;;
       *)

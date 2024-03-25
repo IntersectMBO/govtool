9355c4ea49ec197d5b095eb846d41b28dff3342f
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-03-25 08:59:26 +0100

    [#530] Fix broken Haskell backend developers' configuration
    In response to the user story task requesting the implementation of code
    formatting and checks for the backend to ensure a consistent code style,
    this commit addresses a broken configuration in the Haskell backend. The
    changes made focus on correcting the Haskell backend developer's
    configuration, enabling a more reliable code formatting and checking
    process to be established.
    
    The modifications include fixing the broken configuration by adjusting
    the Haskell backend's default.nix file. This involves fixing the mechanism
    that appends additional libraries and tools required for the project,
    and making necessary adjustments to the project's modifier and overrides
    for proper functionality and adherence to the defined code style.

--- govtool/backend/default.nix
+++ govtool/backend/default.nix
@@ -1,24 +1,29 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  # This is the version of the Haskell compiler we reccommend using.
-  ghcPackages = pkgs.haskell.packages.ghc927;
+  inherit (pkgs.lib.trivial) pipe;
+  inherit (pkgs) haskell;
+  inherit (haskell) lib;
 
-  additionalTools = drv:
-    pkgs.haskell.lib.addBuildTools drv (with ghcPackages; [
+  # This is the version of the Haskell compiler we recommend using.
+  ghcPackages = haskell.packages.ghc927;
+
+  appendLibraries = drv: lib.addExtraLibraries drv (with pkgs; [ lzma zlib ]);
+
+  appendTools = drv:
+    lib.addBuildTools drv (with ghcPackages; [
       cabal-install
       haskell-language-server
-      lzma
       ormolu
-      pkgs.postgresql
-      zlib
     ]);
+
+  useBroken = drv: pipe drv [ lib.markBroken lib.dontCheck ];
+
+  modifier = drv: pipe drv [ appendLibraries appendTools ];
 
   project = ghcPackages.developPackage {
     root = ./.;
-    modifier = additionalTools;
-    overrides = self: super: {
-      openapi3 = pkgs.haskell.lib.dontCheck super.openapi3;
-    };
+    modifier = modifier;
+    overrides = self: super: { openapi3 = useBroken super.openapi3; };
   };
 in project.overrideAttrs (oldAttrs: {
   shellHook = ''

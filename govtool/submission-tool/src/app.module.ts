9703ccfb572f38e750fa9db629938b0fde02b52d
Michał Szałowski <michal.szalowski@gmail.com>
2024-04-01 12:05:38 +0200

    feat: add health check

--- govtool/submission-tool/src/app.module.ts
+++ govtool/submission-tool/src/app.module.ts
@@ -3,13 +3,19 @@ import { HttpModule } from '@nestjs/axios';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
     }),
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- govtool/submission-tool/src/main.ts
+++ govtool/submission-tool/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();

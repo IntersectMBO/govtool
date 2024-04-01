6e92cb32699e13cd7cdb4fdb64afb7b7ad7dedad
Michał Szałowski <michal.szalowski@gmail.com>
2024-04-01 12:29:38 +0200

    feat: add swagger documentation

--- govtool/submission-tool/src/main.ts
+++ govtool/submission-tool/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Submission Tool')
+    .setDescription('The Submission Tool API description')
+    .setVersion('1.0')
+    .addTag('submission')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }

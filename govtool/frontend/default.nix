5483dd3a456b4f3bca2a7556edac563d2c48ccd8
Samuel Leathers <samuel.leathers@iohk.io>
2024-04-18 11:42:47 -0400

    nix: fix frontend
    * adds `js` devshell to fix yarn.lock
    * parameterizes build to pass ENV vars
    * exposes nodeModules, staticSite and devShell in frontend
    * adds overridable env variables when building staticSite

--- govtool/frontend/default.nix
+++ govtool/frontend/default.nix
@@ -1,22 +1,42 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, VITE_BASE_URL ? "http://localhost"
+, VITE_IS_DEV ? "true"
+, VITE_GTM_ID ? ""
+, VITE_SENTRY_DSN ? ""
+, CARDANO_NETWORK ? "sanchonet"
+}:
 let
-  project = pkgs.mkYarnPackage {
-    name = "govtool-frontend";
+  VITE_NETWORK_FLAG = if CARDANO_NETWORK == "mainnet" then "1" else "0";
+  nodeModules = pkgs.mkYarnPackage {
+    name = "govtool-node-modules";
     src = ./.;
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
     nodejs = pkgs.nodejs_18;
   };
-in
-project.overrideAttrs (attrs: {
-  shellHook = ''
-    function warn() { tput setaf $2; echo "$1"; tput sgr0; }
+  staticSite = pkgs.stdenv.mkDerivation {
+    name = "govtool-website";
+    src = ./.;
+    buildInputs = [pkgs.yarn nodeModules];
+    inherit VITE_BASE_URL VITE_IS_DEV VITE_GTM_ID VITE_SENTRY_DSN VITE_NETWORK_FLAG;
+    buildPhase = ''
+      ln -s ${nodeModules}/libexec/voltaire-voting-app/node_modules node_modules
+      yarn build
+    '';
+    installPhase = ''
+      mv dist $out
+    '';
+  };
+  devShell = pkgs.mkShell {
+    shellHook = ''
+      function warn() { tput setaf $2; echo "$1"; tput sgr0; }
 
-    tput bold
-    warn "Welcome to GovTool!" 4
-    warn "This is a frontend development shell." 4
-    warn "Read the ${./README.md} to get more info about this module." 8
-    rm -rf ./node_modules
-    ln -s ${project.out}/libexec/voltaire-voting-app/node_modules ./node_modules
-  '';
-})
+      tput bold
+      warn "Welcome to GovTool!" 4
+      warn "This is a frontend development shell." 4
+      warn "Read the ${./README.md} to get more info about this module." 8
+      rm -rf ./node_modules
+      ln -s ${nodeModules.out}/libexec/voltaire-voting-app/node_modules ./node_modules
+    '';
+  };
+in { inherit nodeModules devShell staticSite; }

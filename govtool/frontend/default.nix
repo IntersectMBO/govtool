c7c0d87fb7a679ce4518e6e310104f9d0521a295
Samuel Leathers <samuel.leathers@iohk.io>
2024-04-20 01:00:20 -0400

    frontend: add nginx webserver
    * creates a customizable nginx webserver for local development
    * adds nix-inclusive to prevent unnecessary rebuilds
    * changes VITE_BASE_URL to not automatically append `/api`

--- govtool/frontend/default.nix
+++ govtool/frontend/default.nix
@@ -1,4 +1,5 @@
 { pkgs ? import <nixpkgs> {}
+, incl
 , VITE_BASE_URL ? "http://localhost"
 , VITE_IS_DEV ? "true"
 , VITE_GTM_ID ? ""
@@ -7,16 +8,26 @@
 }:
 let
   VITE_NETWORK_FLAG = if CARDANO_NETWORK == "mainnet" then "1" else "0";
+  frontendSrc = incl ./. [
+    ./package.json
+    ./yarn.lock
+    ./src
+    ./public
+    ./patches
+    ./vite.config.ts
+    ./index.html
+  ];
+
   nodeModules = pkgs.mkYarnPackage {
     name = "govtool-node-modules";
-    src = ./.;
+    src = frontendSrc;
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
     nodejs = pkgs.nodejs_18;
   };
   staticSite = pkgs.stdenv.mkDerivation {
     name = "govtool-website";
-    src = ./.;
+    src = frontendSrc;
     buildInputs = [pkgs.yarn nodeModules];
     inherit VITE_BASE_URL VITE_IS_DEV VITE_GTM_ID VITE_SENTRY_DSN VITE_NETWORK_FLAG;
     buildPhase = ''
@@ -27,7 +38,33 @@ let
       mv dist $out
     '';
   };
+  webserver = { staticSiteRoot ? staticSite, backendUrl ? "http://localhost:9999" }: let
+    nginxConfig = pkgs.writeText "govtool-nginx.conf" ''
+      daemon off;
+      pid /tmp/govtool-nginx.pid;
+      events {
+      }
+      error_log /dev/stdout info;
+      http {
+        access_log /dev/stdout combined;
+        server {
+          listen 8081;
+          include ${pkgs.nginx}/conf/mime.types;
+          root ${staticSiteRoot}/;
+          index index.html;
+          try_files $uri $uri /index.html;
+          location /api/ {
+            proxy_pass ${backendUrl}/;
+          }
+        }
+      }
+    '';
+  in pkgs.writeScriptBin "govtool-webserver" ''
+    echo "Starting nginx from site root ${staticSiteRoot}... at http://localhost:8081"
+    ${pkgs.nginx}/bin/nginx -c ${nginxConfig} -e /dev/stderr
+  '';
   devShell = pkgs.mkShell {
+    buildInputs = [pkgs.nodejs_18 pkgs.yarn];
     shellHook = ''
       function warn() { tput setaf $2; echo "$1"; tput sgr0; }
 
@@ -39,4 +76,4 @@ let
       ln -s ${nodeModules.out}/libexec/voltaire-voting-app/node_modules ./node_modules
     '';
   };
-in { inherit nodeModules devShell staticSite; }
+in staticSite // { inherit nodeModules devShell staticSite webserver; }

1ffdd3275baed88606625b42677c50445818f782
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-04-24 15:41:59 +0200

    [#849] Add a target to build a docker image for analytics-dashboard
    Included a target in the Makefile to facilitate the creation of a Docker
    image for the analytics-dashboard component. This addition enables the
    generation and pushing of the analytics-dashboard Docker image as part
    of the deployment process. By introducing this functionality, the build
    process now encompasses the analytics-dashboard, supporting the user
    story's aim of configuring the sanchogov.tools sub-domain for the
    analytics service.

---
+++ scripts/govtool/analytics-dashboard.mk
@@ -0,0 +1,20 @@
+common_mk := common.mk
+ifeq ($(origin $(common_mk)), undefined)
+  $(eval $(common_mk) := included)
+  include $(common_mk)
+endif
+
+.DEFAULT_GOAL := push-analytics-dashboard
+
+# image tags
+analytics_dashboard_image_tag := $(shell git log -n 1 --format="%H" -- $(root_dir)/govtool/analytics-dashboard)
+
+.PHONY: build-analytics-dashboard
+build-analytics-dashboard:
+	$(call check_image_on_ecr,analytics-dashboard,$(analytics_dashboard_image_tag)) || \
+	$(docker) build --tag "$(repo_url)/analytics-dashboard:$(analytics_dashboard_image_tag)" $(root_dir)/govtool/analytics-dashboard
+
+.PHONY: push-analytics-dashboard
+push-analytics-dashboard: build-analytics-dashboard
+	$(call check_image_on_ecr,analytics-dashboard,$(analytics_dashboard_image_tag)) || \
+	$(docker) push $(repo_url)/analytics-dashboard:$(analytics_dashboard_image_tag)

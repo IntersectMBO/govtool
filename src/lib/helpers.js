d90805282c6349b4e3f15efd4b73e3ebe7031eb0
Paweł Placzyński <placzynski.pawel@gmail.com>
2024-04-25 13:36:01 +0200

    Squashed 'govtool/analytics-dashboard/' content from commit 9d116a9
    git-subtree-dir: govtool/analytics-dashboard
    git-subtree-split: 9d116a9c785edde019a243cee950eb4e476c633f

---
+++ src/lib/helpers.js
@@ -0,0 +1,4 @@
+// helpers.js
+// This file contains helper functions used throughout the application for performing common tasks.
+// Functions include operations such as date formatting, array and object manipulation, and input validation.
+// Use these functions to keep your code clean and avoid duplicating code in different parts of the application.

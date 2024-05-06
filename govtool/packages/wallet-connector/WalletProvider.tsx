3a689e7e8a0f83308e85487e79a3cda5c4da8cfd
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-05-06 14:23:11 +0200

    add wallet conncetor package

---
+++ govtool/packages/wallet-connector/WalletProvider.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, useContext, useMemo, useState } from "react";
+
+import { WalletService } from "./WalletService";
+import {
+  TWalletAPI,
+  WalletContextProviderProps,
+  WalletConnectorErrors,
+  WalletContextValues,
+} from "./types";
+
+const WalletContext = createContext<WalletContextValues | null>(null);
+
+const WalletProvider = ({ children }: WalletContextProviderProps) => {
+  const [isEnableLoading, setIsEnableLoading] = useState<boolean>(false);
+  const [enableError, setEnableError] = useState<string | null>(null);
+  const [walletAPI, setWalletAPI] = useState<TWalletAPI | null>(null);
+
+  const enableWallet = async (walletName: string): Promise<void> => {
+    setEnableError(null);
+    setIsEnableLoading(true);
+    try {
+      const newWalletAPI = await WalletService.enableWallet(walletName);
+
+      if (newWalletAPI) setWalletAPI(newWalletAPI);
+    } catch (e) {
+      setEnableError(e);
+      throw e;
+    } finally {
+      setIsEnableLoading(false);
+    }
+  };
+
+  const disconnectWallet = () => {
+    setWalletAPI(null);
+  };
+
+  const value = useMemo(
+    () => ({
+      disconnectWallet,
+      enableError,
+      enableWallet,
+      isEnableLoading,
+      walletAPI,
+    }),
+    [disconnectWallet, enableError, enableWallet, isEnableLoading, walletAPI]
+  );
+
+  return (
+    <WalletContext.Provider value={value}>{children}</WalletContext.Provider>
+  );
+};
+
+const useWalletContext = (): WalletContextValues => {
+  const context = useContext(WalletContext);
+  if (!context)
+    throw new Error(
+      WalletConnectorErrors.USE_WALLET_CONTEXT_USED_WITHOUT_PROVIDER
+    );
+
+  return context;
+};
+
+export default { useWalletContext, WalletProvider };
---
+++ govtool/packages/wallet-connector/WalletService.ts
@@ -0,0 +1,37 @@
+import { TWalletAPI, WalletConnectorErrors } from "./types";
+
+export namespace WalletService {
+  export const enableWallet = async (
+    walletName: string
+  ): Promise<TWalletAPI> => {
+    try {
+      if (!window.cardano[walletName].supportedExtensions)
+        throw new Error(
+          WalletConnectorErrors.NO_CIP30_SUPPORT.replace(
+            "%WALLET_NAME%",
+            walletName
+          )
+        );
+
+      if (
+        !window.cardano[walletName].supportedExtensions.some(
+          (item) => item.cip === 95
+        )
+      )
+        throw new Error(
+          WalletConnectorErrors.NO_CIP95_SUPPORT.replace(
+            "%WALLET_NAME%",
+            walletName
+          )
+        );
+
+      const walletApi = await window.cardano[walletName].enable({
+        extensions: [{ cip: 95 }],
+      });
+
+      return walletApi;
+    } catch (e) {
+      throw e;
+    }
+  };
+}

dd85e57ad4e91214912da6b0aa788900c44e99d3
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2024-05-08 16:19:59 +0545

    Refactor build scripts and gh-action

---
+++ tests/test-infrastructure/build-and-deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BASE_IMAGE_NAME=govtool
+export GOVTOOL_TAG="$(git rev-parse HEAD)"
+export PROJECT_NAME=govtool
+export CARDANO_NETWORK=sanchonet
+export BASE_DOMAIN=govtool.cardanoapi.io
+
+. ./scripts/deploy-stack.sh
+
+check_env
+
+# Build images
+./build-images.sh
+function update-service(){
+  docker service update --image "$2" "$1"
+}
+
+if [[ "$1" == "update-images" ]]
+
+  update-service govtool_backend  "$BASE_IMAGE_NAME"/backend:${GOVTOOL_TAG}
+  update-service govtool_frontend "$BASE_IMAGE_NAME"/frontend:${GOVTOOL_TAG}
+  update-service govtool_metadata-validation "$BASE_IMAGE_NAME"/metadata-validation:${GOVTOOL_TAG}
+
+  update-service govaction-loader_backend "$BASE_IMAGE_NAME"/gov-action-loader-frontend:${GOVTOOL_TAG}
+  update-service govaction-loader_frontend "$BASE_IMAGE_NAME"/gov-action-loader-backend:${GOVTOOL_TAG}
+
+elif  [[ $1 == "full" ]]
+  ./deploy stack all
+fi
--- tests/test-infrastructure/build-images.sh
+++ tests/test-infrastructure/build-images.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+set -e
 export BASE_IMAGE_NAME="govtool"
-# build the base image
-docker build  -t "$BASE_IMAGE_NAME"/backend-base -f ../../govtool/backend/Dockerfile.base ../../govtool/backend
+BASE_IMAGE_EXISTS=$(docker images -q "$BASE_IMAGE_NAME"/backend-base 2> /dev/null)
+
+if [ -z "$BASE_IMAGE_EXISTS" ]; then
+  echo "Building the base image..."
+  docker build -t "$BASE_IMAGE_NAME"/backend-base -f ../../govtool/backend/Dockerfile.base ../../govtool/backend
+else
+  echo "Base image already exists. Skipping build."
+fi
+
 docker compose -f ./docker-compose-govtool.yml build
-docker compose -f ./docker-compose.yml build
+docker compose -f ./docker-compose-govaction-loader.yml build

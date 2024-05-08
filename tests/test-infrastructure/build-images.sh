10aa8ea3125b05e0b3aa5b4446ab93f5cb588204
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2024-05-08 16:51:57 +0545

    Remove pipe to /dev/null

--- tests/test-infrastructure/build-images.sh
+++ tests/test-infrastructure/build-images.sh
@@ -2,7 +2,7 @@
 set -e
 set -vx;
 export BASE_IMAGE_NAME="govtool"
-BASE_IMAGE_EXISTS=$(docker images -q "$BASE_IMAGE_NAME"/backend-base 2> /dev/null)
+BASE_IMAGE_EXISTS=$(docker images -q "$BASE_IMAGE_NAME"/backend-base)
 
 if [ -z "$BASE_IMAGE_EXISTS" ]; then
   echo "Building the base image..."

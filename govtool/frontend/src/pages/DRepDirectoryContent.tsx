dd2c5aef66cbbf1423a51a78e9b4bbfbd7ccc6aa
weronika <werawera112@gmail.com>
2024-05-14 13:39:24 +0200

    change condition for more cases

--- govtool/frontend/src/pages/DRepDirectoryContent.tsx
+++ govtool/frontend/src/pages/DRepDirectoryContent.tsx
@@ -65,7 +65,12 @@ export const DRepDirectoryContent: FC<DRepDirectoryContentProps> = ({
   const { dRepData: yourselfDRepList } = useGetDRepListInfiniteQuery({
     searchPhrase: myDRepId,
   });
-  const yourselfDRep = isConnected ? yourselfDRepList?.[0] : undefined;
+
+  const yourselfDRep =
+    !!isConnected &&
+    (debouncedSearchText === myDRepId || debouncedSearchText === "")
+      ? yourselfDRepList?.[0]
+      : undefined;
 
   const {
     dRepData: dRepList,

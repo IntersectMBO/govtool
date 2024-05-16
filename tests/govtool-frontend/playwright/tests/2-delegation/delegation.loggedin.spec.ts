0791e35501c8b1c9abb7e080d7261c14121cddb2
Nabin Kawan <nabin.kawan.9@gmail.com>
2024-05-16 21:12:13 +0545

    test: Delegation (logged in)

--- tests/govtool-frontend/playwright/tests/2-delegation/delegation.loggedin.spec.ts
+++ tests/govtool-frontend/playwright/tests/2-delegation/delegation.loggedin.spec.ts
@@ -1,49 +1,48 @@
-import { user01Wallet } from "@constants/staticWallets";
+import { dRep01Wallet, user01Wallet } from "@constants/staticWallets";
 import { test } from "@fixtures/walletExtension";
-import DelegationPage from "@pages/delegationPage";
+import convertBufferToHex from "@helpers/convertBufferToHex";
+import { ShelleyWallet } from "@helpers/crypto";
+import extractDRepFromWallet from "@helpers/shellyWallet";
+import DRepDirectoryPage from "@pages/dRepDirectoryPage";
 import { expect } from "@playwright/test";
 
 test.use({ storageState: ".auth/user01.json", wallet: user01Wallet });
 
-test("2B. Should access delegation to dRep page @smoke @fast", async ({
-  page,
-}) => {
+test("2B. Should access delegation to dRep page", async ({ page }) => {
   await page.goto("/");
 
-  await page.getByTestId("delegate-button").click(); // BUG incorrect test ID
+  await page.getByTestId("view-drep-directory-button").click();
   await expect(
-    page.getByRole("navigation").getByText("DRep Directory"),
+    page.getByRole("navigation").getByText("DRep Directory")
   ).toBeVisible();
 });
 
-// Skipped: No need to insert dRep id to delegate
-test.skip("2I. Should check validity of DRep Id @slow", async ({ page }) => {
-  // const urlToIntercept = "**/utxo?**";
-  // const invalidDRepId = generateRandomDRepId();
-  // const validDRepId = dRep01Wallet.dRepId;
-  // // Invalidity checks
-  // const delegationPage = new DelegationPage(page);
-  // await delegationPage.goto();
-  // await delegationPage.delegateToDRep(invalidDRepId);
-  // await expect(delegationPage.delegationErrorModal).toBeVisible();
-  // await delegationPage.resetDRepForm();
-  // // Validity checks
-  // await delegationPage.dRepInput.fill(validDRepId);
-  // await delegationPage.delegateBtn.click();
-  // const response = await page.waitForResponse(urlToIntercept);
-  // expect(response.body.length).toEqual(0);
+test("2I. Should check validity of DRep Id", async ({ page }) => {
+  const dRepDirectory = new DRepDirectoryPage(page);
+  await dRepDirectory.goto();
+
+  await dRepDirectory.searchInput.fill(dRep01Wallet.dRepId);
+  await expect(dRepDirectory.getDRepCard(dRep01Wallet.dRepId)).toHaveText(
+    dRep01Wallet.dRepId
+  );
+
+  const wallet = await ShelleyWallet.generate();
+  const invalidDRepId = extractDRepFromWallet(wallet);
+
+  await dRepDirectory.searchInput.fill(invalidDRepId);
+  await expect(dRepDirectory.getDRepCard(invalidDRepId)).not.toBeVisible();
 });
 
-test("2D. Verify Delegation Behavior in Connected State @smoke @fast", async ({
-  page,
-}) => {
-  const delegationPage = new DelegationPage(page);
-  await delegationPage.goto();
+test("2D. Verify Delegation Behavior in Connected State", async ({ page }) => {
+  const dRepDirectoryPage = new DRepDirectoryPage(page);
+  await dRepDirectoryPage.goto();
 
-  // Verifying delegation options
-  await delegationPage.delegationOptionsDropdown.click();
-  await expect(delegationPage.signalNoConfidenceCard).toBeVisible();
-  await expect(delegationPage.abstainDelegationCard).toBeVisible();
+  // Verifying automatic delegation options
+  await dRepDirectoryPage.automaticDelegationOptionsDropdown.click();
+  await expect(dRepDirectoryPage.abstainDelegationCard).toBeVisible();
+  await expect(dRepDirectoryPage.signalNoConfidenceCard).toBeVisible();
 
-  expect(await delegationPage.delegateBtns.count()).toBeGreaterThanOrEqual(2);
+  expect(await dRepDirectoryPage.delegateBtns.count()).toBeGreaterThanOrEqual(
+    2
+  );
 });

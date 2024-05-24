d5276ed55b1ce789c5c6edeecc20adf9ab62fd3c
niraj <duwalkneerose@gmail.com>
2024-05-24 08:17:28 +0545

    Add the missing allure story categorized.

--- tests/govtool-frontend/playwright/tests/dRep.setup.ts
+++ tests/govtool-frontend/playwright/tests/dRep.setup.ts
@@ -1,11 +1,17 @@
 import environments from "@constants/environments";
 import { dRepWallets } from "@constants/staticWallets";
+import { setAllureEpic, setAllureStory } from "@helpers/allure";
 import { pollTransaction } from "@helpers/transaction";
 import { expect, test as setup } from "@playwright/test";
 import kuberService from "@services/kuberService";
 import fetch = require("node-fetch");
 
 setup.describe.configure({ timeout: environments.txTimeOut });
+
+setup.beforeEach(async () => {
+  await setAllureEpic("Setup");
+  await setAllureStory("Register DRep");
+});
 
 dRepWallets.forEach((wallet) => {
   setup(`Register DRep of wallet: ${wallet.address}`, async () => {

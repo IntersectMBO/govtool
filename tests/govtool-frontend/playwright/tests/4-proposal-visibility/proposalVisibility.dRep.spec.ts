9933bf00f36f4f2bcf92a3a4b667f4fc0969f431
niraj <duwalkneerose@gmail.com>
2024-05-28 12:04:49 +0545

    test: Add check vote count
    Add test 4K for check vote count on disconnected state and refine test 4G for check vote count on connected state

--- tests/govtool-frontend/playwright/tests/4-proposal-visibility/proposalVisibility.dRep.spec.ts
+++ tests/govtool-frontend/playwright/tests/4-proposal-visibility/proposalVisibility.dRep.spec.ts
@@ -60,49 +60,54 @@ test.describe("Temporary DReps", async () => {
   });
 });
 
-test("4G. Should display correct vote counts on governance details page for DRep", async ({
-  page,
-}) => {
-  const responsesPromise = Object.keys(FilterOption).map((filterKey) =>
-    page.waitForResponse((response) =>
-      response.url().includes(`&type[]=${FilterOption[filterKey]}`)
-    )
-  );
+test.describe("Check vote count", () => {
+  test.use({ storageState: ".auth/dRep01.json", wallet: dRep01Wallet });
 
-  const governanceActionsPage = new GovernanceActionsPage(page);
-  await governanceActionsPage.goto();
-  const responses = await Promise.all(responsesPromise);
-  const proposals: IProposal[] = (
-    await Promise.all(
-      responses.map(async (response) => {
-        const data = await response.json();
-        return data.elements;
-      })
-    )
-  ).flat();
+  test("4G. Should display correct vote counts on governance details page for DRep", async ({
+    page,
+  }) => {
+    const responsesPromise = Object.keys(FilterOption).map((filterKey) =>
+      page.waitForResponse((response) =>
+        response.url().includes(`&type[]=${FilterOption[filterKey]}`)
+      )
+    );
 
-  expect(proposals.length, "No proposals found!").toBeGreaterThan(0);
+    const governanceActionsPage = new GovernanceActionsPage(page);
+    await governanceActionsPage.goto();
 
-  const proposalToCheck = proposals[0];
-  const govActionDetailsPage =
-    await governanceActionsPage.viewProposal(proposalToCheck);
-  await govActionDetailsPage.showVotesBtn.click();
+    const responses = await Promise.all(responsesPromise);
+    const proposals: IProposal[] = (
+      await Promise.all(
+        responses.map(async (response) => {
+          const data = await response.json();
+          return data.elements;
+        })
+      )
+    ).flat();
 
-  await expect(
-    page
-      .getByText("yes₳")
-      .getByText(`₳ ${lovelaceToAda(proposalToCheck.yesVotes)}`)
-  ).toBeVisible();
-  await expect(
-    page
-      .getByText("abstain₳")
-      .getByText(`₳ ${lovelaceToAda(proposalToCheck.abstainVotes)}`)
-  ).toBeVisible();
-  await expect(
-    page
-      .getByText("no₳")
-      .getByText(`₳ ${lovelaceToAda(proposalToCheck.noVotes)}`)
-  ).toBeVisible();
+    expect(proposals.length, "No proposals found!").toBeGreaterThan(0);
+
+    const proposalToCheck = proposals[0];
+    const govActionDetailsPage =
+      await governanceActionsPage.viewProposal(proposalToCheck);
+    await govActionDetailsPage.showVotesBtn.click();
+
+    await expect(
+      page
+        .getByText("yes₳")
+        .getByText(`₳ ${lovelaceToAda(proposalToCheck.yesVotes)}`)
+    ).toBeVisible();
+    await expect(
+      page
+        .getByText("abstain₳")
+        .getByText(`₳ ${lovelaceToAda(proposalToCheck.abstainVotes)}`)
+    ).toBeVisible();
+    await expect(
+      page
+        .getByText("no₳")
+        .getByText(`₳ ${lovelaceToAda(proposalToCheck.noVotes)}`)
+    ).toBeVisible();
+  });
 });
 
 test("4F. Should Disable DRep functionality upon wallet disconnection on governance actions page", async ({

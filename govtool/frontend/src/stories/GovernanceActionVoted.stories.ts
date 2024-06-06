30286d815b392772cb0d298261f84f31b390ceea
Michał Szałowski <michal.szalowski@gmail.com>
2024-06-06 14:34:54 +0200

    chore: fix tsc errors

--- govtool/frontend/src/stories/GovernanceActionVoted.stories.ts
+++ govtool/frontend/src/stories/GovernanceActionVoted.stories.ts
@@ -64,11 +64,8 @@ export const GovernanceVotedOnCardComponent: Story = {
       proposal: {
         createdEpochNo: 232,
         expiryEpochNo: 323,
-        metadataStatus: {
-          raw: { status: undefined, valid: true },
-          status: undefined,
-          valid: false,
-        },
+        metadataStatus: null,
+        metadataValid: true,
         createdDate: "1970-01-01T00:00:00Z",
         expiryDate: "1970-02-01T00:00:00Z",
         id: "exampleId",
@@ -105,11 +102,8 @@ export const GovernanceVotedOnCardAbstain: Story = {
       proposal: {
         createdEpochNo: 232,
         expiryEpochNo: 323,
-        metadataStatus: {
-          raw: { status: undefined, valid: true },
-          status: undefined,
-          valid: false,
-        },
+        metadataStatus: null,
+        metadataValid: true,
         createdDate: "1970-01-01T00:00:00Z",
         expiryDate: "1970-02-01T00:00:00Z",
         id: "exampleId",
@@ -147,11 +141,8 @@ export const GovernanceVotedOnCardYes: Story = {
       proposal: {
         createdEpochNo: 232,
         expiryEpochNo: 323,
-        metadataStatus: {
-          raw: { status: undefined, valid: true },
-          status: undefined,
-          valid: false,
-        },
+        metadataStatus: null,
+        metadataValid: true,
         createdDate: "1970-01-01T00:00:00Z",
         expiryDate: "1970-02-01T00:00:00Z",
         id: "exampleId",
@@ -189,11 +180,8 @@ export const GovernanceVotedOnCardNo: Story = {
       proposal: {
         createdEpochNo: 232,
         expiryEpochNo: 323,
-        metadataStatus: {
-          raw: { status: undefined, valid: true },
-          status: undefined,
-          valid: false,
-        },
+        metadataStatus: null,
+        metadataValid: true,
         createdDate: "1970-01-01T00:00:00Z",
         expiryDate: "1970-02-01T00:00:00Z",
         id: "exampleId",

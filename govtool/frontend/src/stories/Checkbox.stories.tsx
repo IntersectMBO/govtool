6033b44031d62cc58a31237f60faac802c327c4d
Nabin Kawan <nabin.kawan.9@gmail.com>
2024-06-10 15:11:12 +0545

    Add interaction tests for checkbox

--- govtool/frontend/src/stories/Checkbox.stories.tsx
+++ govtool/frontend/src/stories/Checkbox.stories.tsx
@@ -1,6 +1,8 @@
+import { expect } from "@storybook/jest";
 import type { Meta, StoryFn } from "@storybook/react";
 
 import { Field } from "@molecules";
+import { within } from "@storybook/testing-library";
 import { ComponentProps } from "react";
 
 const meta = {
@@ -19,19 +21,43 @@ const Template: StoryFn<ComponentProps<typeof Field.Checkbox>> = (args) => (
 );
 
 export const Default = Template.bind({});
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByRole("checkbox")).not.toBeChecked();
+};
 
 export const WithLabel = Template.bind({});
 WithLabel.args = {
   label: "Label",
+};
+WithLabel.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByRole("checkbox")).not.toBeChecked();
+  expect(canvas.getByText("Label")).toBeVisible();
 };
 
 export const Error = Template.bind({});
 Error.args = {
   errorMessage: "Error message",
 };
+Error.play = async ({ canvasElement, args }) => {
+  const canvas = within(canvasElement);
+  const errorId = args.errorMessage!.toLowerCase().split(" ").join("-");
+
+  expect(canvas.getByRole("checkbox")).not.toBeChecked();
+  expect(canvas.getByTestId(`${errorId}-error`)).toBeVisible();
+};
 
 export const ErrorAndLabel = Template.bind({});
 ErrorAndLabel.args = {
   errorMessage: "Error message",
   label: "Label",
+};
+ErrorAndLabel.play = async ({ canvasElement, args }) => {
+  const canvas = within(canvasElement);
+  const errorId = args.errorMessage!.toLowerCase().split(" ").join("-");
+
+  expect(canvas.getByRole("checkbox")).not.toBeChecked();
+  expect(canvas.getByText("Label")).toBeVisible();
+  expect(canvas.getByTestId(`${errorId}-error`)).toBeVisible();
 };

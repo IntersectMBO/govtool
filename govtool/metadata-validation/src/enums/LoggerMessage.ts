e20ffea5b120675f9495030f417393a9d54ba1e4
Michał Szałowski <michal.szalowski@gmail.com>
2024-06-13 16:55:11 +0200

    chore(#1281): increase level of logs of metada validation service

---
+++ govtool/metadata-validation/src/enums/LoggerMessage.ts
@@ -0,0 +1,7 @@
+export enum LoggerMessage {
+  METADATA_VALIDATION_ERROR = 'Metadata validation error',
+  METADATA_DATA = 'Metadata data',
+  CANNOT_GET_METADATA_URL = 'Cannot get metadata from URL',
+  PARSED_METADATA_BODY = 'Parsed metadata body',
+  CANNOT_PARSE_METADATA_BODY = 'Cannot parse metadata body',
+}

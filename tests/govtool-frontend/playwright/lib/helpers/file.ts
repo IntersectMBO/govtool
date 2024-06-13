fc16116deb22ea57066f37fe707a202b75396eab
Nabin Kawan <nabin.kawan.9@gmail.com>
2024-06-13 12:04:58 +0545

    chore: Add helper for create file

---
+++ tests/govtool-frontend/playwright/lib/helpers/file.ts
@@ -0,0 +1,21 @@
+import { writeFile } from "fs";
+
+const path = require("path");
+
+const baseFilePath = path.resolve(__dirname, "./_mock");
+
+export async function createFile(fileName: string, data?: any) {
+  await new Promise<void>((resolve, reject) =>
+    writeFile(
+      `${baseFilePath}/${fileName}`,
+      JSON.stringify(data, null, 2),
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+    )
+  );
+}

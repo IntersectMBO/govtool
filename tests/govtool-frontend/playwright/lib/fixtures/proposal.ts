17ac9ccd341efe7e620b206ed1fc46120e23eecb
niraj <duwalkneerose@gmail.com>
2024-06-14 14:29:56 +0545

    chore: Add pollEnabled option on proposal fixture

--- tests/govtool-frontend/playwright/lib/fixtures/proposal.ts
+++ tests/govtool-frontend/playwright/lib/fixtures/proposal.ts
@@ -6,10 +6,13 @@ import ProposalDiscussionPage from "@pages/proposalDiscussionPage";
 
 type TestOptions = {
   proposalId: number;
+  pollEnabled: boolean;
 };
 
 export const test = base.extend<TestOptions>({
-  proposalId: async ({ page, browser }, use) => {
+  pollEnabled: [false, { option: true }],
+
+  proposalId: async ({ page, browser, pollEnabled }, use) => {
     // setup
     const proposalPage = await createNewPageWithWallet(browser, {
       storageState: ".auth/proposal01.json",
@@ -19,11 +22,15 @@ export const test = base.extend<TestOptions>({
     const proposalDiscussionPage = new ProposalDiscussionPage(proposalPage);
     await proposalDiscussionPage.goto();
     const proposalId = await proposalDiscussionPage.createProposal();
+    const proposalDetailsPage = new ProposalDiscussionDetailsPage(proposalPage);
+
+    if (pollEnabled) {
+      await proposalDetailsPage.addPollBtn.click();
+    }
 
     await use(proposalId);
 
     // cleanup
-    const proposalDetailsPage = new ProposalDiscussionDetailsPage(proposalPage);
     await proposalDetailsPage.goto(proposalId);
     await proposalDetailsPage.deleteProposal();
   },

dbb00ed2a69218fbf5c39b8c39a5aed0bb617069
Michał Szałowski <michal.szalowski@gmail.com>
2024-06-20 14:47:17 +0200

    feat: pass govtool metadata validator to pdf pillar

--- govtool/frontend/src/types/@intersect.mbo.d.ts
+++ govtool/frontend/src/types/@intersect.mbo.d.ts
@@ -1,10 +1,32 @@
-type PDFProps = {
+enum MetadataValidationStatus {
+  URL_NOT_FOUND = "URL_NOT_FOUND",
+  INVALID_JSONLD = "INVALID_JSONLD",
+  INVALID_HASH = "INVALID_HASH",
+  INCORRECT_FORMAT = "INCORRECT_FORMAT",
+}
+
+type ProposalDiscussionProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   walletAPI: any;
   pathname: string;
   locale?: string;
+  validateMetadata: ({
+    url,
+    hash,
+    standard,
+  }: {
+    url: string;
+    hash: string;
+    standard: "CIP108";
+  }) => Promise<
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    | { status?: MetadataValidationStatus; metadata?: any; valid: boolean }
+    | undefined
+  >;
 };
 
 declare module "@intersect.mbo/pdf-ui/cjs" {
-  export default function PDF(props: PDFProps): JSX.Element;
+  export default function ProposalDiscussion(
+    props: ProposalDiscussionProps,
+  ): JSX.Element;
 }

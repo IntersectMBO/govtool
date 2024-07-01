7e43f4a95c363b851c505f6bccbd2096541e5078
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-07-01 10:22:36 +0200

    fix Sentry catch exceptions

--- govtool/frontend/src/main.tsx
+++ govtool/frontend/src/main.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  createRoutesFromChildren,
-  matchRoutes,
-  useLocation,
-  useNavigationType,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import TagManager from "react-gtm-module";
@@ -34,42 +28,10 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   environment: import.meta.env.VITE_APP_ENV,
   release: version,
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
-        React.useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes,
-      ),
-    }),
-    new Sentry.Replay(),
-  ],
-
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
-
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-});
-
-Sentry.addGlobalEventProcessor((event) => {
-  window.dataLayer = window.dataLayer || [];
-
-  const errorMessage =
-    (event.exception &&
-      event.exception.values &&
-      event.exception.values[0] &&
-      event.exception.values[0].value) ||
-    "Unknown Error";
-
-  window.dataLayer.push({
-    event: "sentryEvent",
-    sentryEventId: event.event_id || "default_event_id",
-    sentryErrorMessage: errorMessage,
-  });
-
-  return event;
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

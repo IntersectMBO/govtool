67f12cc9a383469cdf1dde914943b9f6808221b0
niraj <duwalkneerose@gmail.com>
2024-07-04 11:57:01 +0545

    chore: Migrate 7J and 7K test from proposal submission functionality to proposal submission loggedin

--- tests/govtool-frontend/playwright/tests/7-proposal-submission/proposalSubmissionFunctionality.proposal.spec.ts
+++ tests/govtool-frontend/playwright/tests/7-proposal-submission/proposalSubmissionFunctionality.proposal.spec.ts
@@ -1,12 +1,10 @@
 import environments from "@constants/environments";
 import { proposal01Wallet } from "@constants/staticWallets";
 import { createTempUserAuth } from "@datafactory/createAuth";
-import { faker } from "@faker-js/faker";
 import { test } from "@fixtures/proposal";
 import { setAllureEpic } from "@helpers/allure";
 import { createNewPageWithWallet } from "@helpers/page";
 import { waitForTxConfirmation } from "@helpers/transaction";
-import { invalid } from "@mock/index";
 import ProposalDiscussionDetailsPage from "@pages/proposalDiscussionDetailsPage";
 import ProposalSubmissionPage from "@pages/proposalSubmissionPage";
 import { expect } from "@playwright/test";
@@ -58,45 +56,4 @@ test("7H. should submit a proposal", async ({ page, browser }, testInfo) => {
   });
 
   await waitForTxConfirmation(userPage);
-});
-
-test.describe("Metadata anchor validation", () => {
-  test("7J_1. Should accept valid metadata anchor on proposal submission", async ({
-    page,
-  }) => {
-    for (let i = 0; i < 100; i++) {
-      await proposalSubmissionPage.metadataUrlInput.fill(faker.internet.url());
-      await expect(page.getByTestId("invalid-url-error")).toBeHidden();
-    }
-  });
-
-  test("7J_2. Should reject invalid metadata anchor on proposal submission", async ({
-    page,
-  }) => {
-    for (let i = 0; i < 100; i++) {
-      await proposalSubmissionPage.metadataUrlInput.fill(invalid.url());
-      await expect(page.getByTestId("invalid-url-error")).toBeVisible();
-    }
-
-    const sentenceWithoutSpace = faker.lorem
-      .sentence(128)
-      .replace(/[\s.]/g, "");
-    const metadataAnchorGreaterThan128Bytes =
-      faker.internet.url({ appendSlash: true }) + sentenceWithoutSpace;
-
-    await proposalSubmissionPage.metadataUrlInput.fill(
-      metadataAnchorGreaterThan128Bytes
-    );
-
-    await expect(page.getByTestId("invalid-url-error")).toBeVisible();
-  });
-});
-
-test("7K. Should reject invalid proposal metadata", async ({ page }) => {
-  await proposalSubmissionPage.metadataUrlInput.fill(invalid.url());
-  await proposalSubmissionPage.submitBtn.click();
-
-  await expect(page.getByTestId("url-error-modal-title")).toHaveText(
-    /the url you entered cannot be found/i
-  );
 });

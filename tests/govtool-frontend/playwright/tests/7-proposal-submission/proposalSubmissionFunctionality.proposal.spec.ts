a3292c0db5249b649f78e66c9744a52826b2d04c
niraj <duwalkneerose@gmail.com>
2024-07-04 09:57:42 +0545

    chore: Update metadata anchor validation test
    Change metadata anchor validatation test from 7K to 7J and update its flow according to userstories

--- tests/govtool-frontend/playwright/tests/7-proposal-submission/proposalSubmissionFunctionality.proposal.spec.ts
+++ tests/govtool-frontend/playwright/tests/7-proposal-submission/proposalSubmissionFunctionality.proposal.spec.ts
@@ -1,16 +1,30 @@
 import environments from "@constants/environments";
+import { proposal01Wallet } from "@constants/staticWallets";
 import { createTempUserAuth } from "@datafactory/createAuth";
-import { test } from "@fixtures/walletExtension";
+import { faker } from "@faker-js/faker";
+import { test } from "@fixtures/proposal";
 import { setAllureEpic } from "@helpers/allure";
 import { createNewPageWithWallet } from "@helpers/page";
 import { waitForTxConfirmation } from "@helpers/transaction";
+import { invalid } from "@mock/index";
+import ProposalDiscussionDetailsPage from "@pages/proposalDiscussionDetailsPage";
 import ProposalSubmissionPage from "@pages/proposalSubmissionPage";
 import { expect } from "@playwright/test";
 import { ProposalCreateRequest, ProposalType } from "@types";
 import walletManager from "lib/walletManager";
 
-test.beforeEach(async () => {
+let proposalSubmissionPage: ProposalSubmissionPage;
+test.use({ storageState: ".auth/proposal01.json", wallet: proposal01Wallet });
+
+test.beforeEach(async ({ page, proposalId, browser }) => {
   await setAllureEpic("7. Proposal submission");
+  const proposalDiscussionDetailsPage = new ProposalDiscussionDetailsPage(page);
+  await proposalDiscussionDetailsPage.goto(proposalId);
+  await proposalDiscussionDetailsPage.verifyIdentityBtn.click();
+  await proposalDiscussionDetailsPage.submitAsGABtn.click();
+  proposalSubmissionPage = new ProposalSubmissionPage(page);
+  await page.click("input#submission-checkbox"); // BUG missing test id
+  await page.getByRole("button", { name: "Continue" }).click();
 });
 
 test("7H. should submit a proposal", async ({ page, browser }, testInfo) => {
@@ -41,4 +55,36 @@ test("7H. should submit a proposal", async ({ page, browser }, testInfo) => {
   });
 
   await waitForTxConfirmation(userPage);
+});
+
+test.describe("Metadata anchor validation", () => {
+  test("7J_1. Should accept valid metadata anchor on proposal submission", async ({
+    page,
+  }) => {
+    for (let i = 0; i < 100; i++) {
+      await proposalSubmissionPage.metadataUrlInput.fill(faker.internet.url());
+      await expect(page.getByTestId("invalid-url-error")).toBeHidden();
+    }
+  });
+
+  test("7J_2. Should reject invalid metadata anchor on proposal submission", async ({
+    page,
+  }) => {
+    for (let i = 0; i < 100; i++) {
+      await proposalSubmissionPage.metadataUrlInput.fill(invalid.url());
+      await expect(page.getByTestId("invalid-url-error")).toBeVisible();
+    }
+
+    const sentenceWithoutSpace = faker.lorem
+      .sentence(128)
+      .replace(/[\s.]/g, "");
+    const metadataAnchorGreaterThan128Bytes =
+      faker.internet.url({ appendSlash: true }) + sentenceWithoutSpace;
+
+    await proposalSubmissionPage.metadataUrlInput.fill(
+      metadataAnchorGreaterThan128Bytes
+    );
+
+    await expect(page.getByTestId("invalid-url-error")).toBeVisible();
+  });
 });

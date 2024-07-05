636fc16a5464a3b40b2b5d81823e384d8725c1fe
niraj <duwalkneerose@gmail.com>
2024-07-05 09:51:23 +0545

    chore: Update create proposal logic

--- tests/govtool-frontend/playwright/lib/pages/proposalSubmissionPage.ts
+++ tests/govtool-frontend/playwright/lib/pages/proposalSubmissionPage.ts
@@ -352,33 +352,20 @@ export default class ProposalSubmissionPage {
   }
 
   async createProposal(): Promise<number> {
+    await this.addLinkBtn.click();
     const receivingAddr = (await ShelleyWallet.generate()).rewardAddressBech32(
       0
     );
 
-    const proposalRequest: ProposalCreateRequest = {
-      proposal_links: [
-        {
-          prop_link: faker.internet.url(),
-          prop_link_text: faker.internet.displayName(),
-        },
-      ],
-      gov_action_type_id: 1,
-      prop_name: faker.company.name(),
-      prop_abstract: faker.lorem.paragraph(2),
-      prop_motivation: faker.lorem.paragraph(2),
-      prop_rationale: faker.lorem.paragraph(2),
-      prop_receiving_address: receivingAddr,
-      prop_amount: faker.number.int({ min: 100, max: 1000 }).toString(),
-      is_draft: false,
-    };
-
-    await this.proposalCreateBtn.click();
+    const proposalRequest: ProposalCreateRequest =
+      this.generateValidProposalFormFields(
+        ProposalType.treasury,
+        false,
+        receivingAddr
+      );
+    await this.fillupForm(proposalRequest);
     await this.continueBtn.click();
-
-    await this.validateForm(proposalRequest);
-    await this.continueBtn.click();
-    await this.page.getByRole("button", { name: "Submit" }).click();
+    await this.submitBtn.click();
 
     // Wait for redirection to `proposal-discussion-details` page
     await this.page.waitForTimeout(2_000);

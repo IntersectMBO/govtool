b559a0660c96ffb9cfa1dc6e5620b694a14e8cfd
niraj <duwalkneerose@gmail.com>
2024-08-01 17:06:21 +0545

    chore: update dashboard learn more urls

--- tests/govtool-frontend/playwright/lib/constants/docsUrl.ts
+++ tests/govtool-frontend/playwright/lib/constants/docsUrl.ts
@@ -1,10 +1,10 @@
 import environments from "./environments";
 
-export const DELEGATION_DOC_URL = `${environments.docsUrl}/faqs/ways-to-use-your-voting-power`;
-export const REGISTER_DREP_DOC_URL = `${environments.docsUrl}/faqs/what-does-it-mean-to-register-as-a-drep`;
+export const DELEGATION_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/delegating`;
+export const REGISTER_DREP_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/dreps`;
 export const DIRECT_VOTER_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/direct-voting`;
-export const GOVERNANCE_ACTION_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/governance-actions/governance-actions-how-to-vote`;
-export const PROPOSE_GOVERNANCE_ACTION_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/governance-actions`;
+export const GOVERNANCE_ACTION_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/governance-actions`;
+export const PROPOSE_GOVERNANCE_ACTION_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/governance-actions/propose-a-governance-action`;
 export const ABSTAIN_VOTE_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/delegating/abstain-from-every-vote`;
 export const SIGNAL_NO_CONFIDENCE_VOTE_DOC_URL = `${environments.docsUrl}/how-to-use-the-govtool/using-govtool/delegating/signal-no-confidence-on-every-vote`;
 export const FAQS_DOC_URL = `${environments.docsUrl}/faqs`;

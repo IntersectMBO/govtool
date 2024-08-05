88ddda6475511a4c3bfd62d4fefeb277a0315dd5
niraj <duwalkneerose@gmail.com>
2024-08-05 18:47:22 +0545

    chore: add mock metadata with random value

--- tests/govtool-frontend/playwright/lib/_mock/index.ts
+++ tests/govtool-frontend/playwright/lib/_mock/index.ts
@@ -113,4 +113,82 @@ export const valid = {
 
     return username;
   },
+  metadata: () => {
+    return {
+      "@context": {
+        "@language": "en-us",
+        CIP100:
+          "https://github.com/cardano-foundation/CIPs/blob/master/CIP-0100/README.md#",
+        CIPQQQ:
+          "https://github.com/cardano-foundation/CIPs/blob/master/CIP-QQQ/README.md#",
+        hashAlgorithm: "CIP100:hashAlgorithm",
+        body: {
+          "@id": "CIPQQQ:body",
+          "@context": {
+            references: {
+              "@id": "CIPQQQ:references",
+              "@container": "@set",
+              "@context": {
+                GovernanceMetadata: "CIP100:GovernanceMetadataReference",
+                Other: "CIP100:OtherReference",
+                label: "CIP100:reference-label",
+                uri: "CIP100:reference-uri",
+                referenceHash: {
+                  "@id": "CIPQQQ:referenceHash",
+                  "@context": {
+                    hashDigest: "CIPQQQ:hashDigest",
+                    hashAlgorithm: "CIP100:hashAlgorithm",
+                  },
+                },
+              },
+            },
+            dRepName: "CIPQQQ:dRepName",
+            bio: "CIPQQQ:bio",
+            email: "CIPQQQ:email",
+          },
+        },
+        authors: {
+          "@id": "CIP100:authors",
+          "@container": "@set",
+          "@context": {
+            name: "http://xmlns.com/foaf/0.1/name",
+            witness: {
+              "@id": "CIP100:witness",
+              "@context": {
+                witnessAlgorithm: "CIP100:witnessAlgorithm",
+                publicKey: "CIP100:publicKey",
+                signature: "CIP100:signature",
+              },
+            },
+          },
+        },
+      },
+      authors: [],
+      hashAlgorithm: {
+        "@value": "blake2b-256",
+      },
+      body: {
+        bio: {
+          "@value": faker.lorem.sentences(),
+        },
+        dRepName: {
+          "@value": faker.person.firstName(),
+        },
+        email: {
+          "@value": faker.internet.email(),
+        },
+        references: [
+          {
+            "@type": "Other",
+            label: {
+              "@value": "Label",
+            },
+            uri: {
+              "@value": faker.internet.url(),
+            },
+          },
+        ],
+      },
+    };
+  },
 };

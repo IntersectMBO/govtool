d77ba64c7f15f24f59597d2cbc45429376a61382
niraj <duwalkneerose@gmail.com>
2024-08-16 12:31:03 +0545

    chore: update deprecated set-output command to use GITHUB_OUTPUT environment variable

--- tests/scripts/generate_report_details.sh
+++ tests/scripts/generate_report_details.sh
@@ -7,5 +7,6 @@ else
   latest_number=$(echo "$gh_pages_content" | grep -Eo '[0-9]+' | sort -nr | head -n 1)
 fi
 
-echo "::set-output name=report_number::$((latest_number+1))"
-echo "::set-output name=report_url::https://$(dirname "$GH_PAGES").github.io/$(basename "$GH_PAGES")/$REPORT_NAME"
+echo "report_number=$((latest_number+1))" >> $GITHUB_OUTPUT
+echo "report_url=https://$(dirname "$GH_PAGES").github.io/$(basename "$GH_PAGES")/$REPORT_NAME" >> $GITHUB_OUTPUT
+

f84bd48d77f010cc9212da2c4ec30bff15789f74
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2024-08-17 16:52:29 +0545

    Log docker command

--- tests/test-infrastructure/build-images.sh
+++ tests/test-infrastructure/build-images.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 set -e
+
+# Define a function to log and execute Docker commands
+docker_() {
+  local cmd="$*"
+  echo docker "$cmd"
+  docker $cmd 
+}
+
 export BASE_IMAGE_NAME="govtool"
+
 BASE_IMAGE_EXISTS=$(docker images -q "$BASE_IMAGE_NAME"/backend-base)
 
 if [ -z "$BASE_IMAGE_EXISTS" ]; then
   echo "Building the base image..."
-  docker build -t "$BASE_IMAGE_NAME"/backend-base -f ../../govtool/backend/Dockerfile.base ../../govtool/backend
+  docker_ build -t "$BASE_IMAGE_NAME"/backend-base -f ../../govtool/backend/Dockerfile.base ../../govtool/backend
 else
   echo "Base image already exists. Skipping build."
 fi
 
-docker compose -f ./docker-compose-govtool.yml build
-docker compose -f ./docker-compose-govaction-loader.yml build
-docker compose -f ./docker-compose-test.yml build
+docker_ compose -f ./docker-compose-govtool.yml build
+docker_ compose -f ./docker-compose-govaction-loader.yml build
+docker_ compose -f ./docker-compose-test.yml build

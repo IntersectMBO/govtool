cea87fb198a41b60189d3f3dae649a2e8b93e80d
Reeshav Acharya <reeshav99acharya@gmail.com>
2024-09-03 12:58:08 +0545

    FIx api network selection

--- gov-action-loader/backend/app/network.py
+++ gov-action-loader/backend/app/network.py
@@ -5,7 +5,7 @@ NETWORKS = {"preview", "preprod", "sanchonet"}
 
 def get_api_url (network): 
     network_l_case = network.lower()
-    if network_l_case == "sancho" or "sanchonet":
+    if network_l_case == "sancho" or network_l_case == "sanchonet":
         return settings.kuber_api_url_sancho
     elif network_l_case == "preview": 
         return settings.kuber_api_url_preview

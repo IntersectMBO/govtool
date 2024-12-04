6e04854a1ac195ad3ef813f634642f13257d4f40
Niraj <duwalkneerose@gmail.com>
2024-12-04 13:53:33 +0545

    chore: set kuber api url and faucet url based on network

--- tests/govtool-backend/config.py
+++ tests/govtool-backend/config.py
@@ -11,5 +11,7 @@ dotenv.load_dotenv()
 
 RECORD_METRICS_API = os.getenv("RECORD_METRICS_API")
 METRICS_API_SECRET = os.getenv("METRICS_API_SECRET")
-KUBER_API_URL = os.getenv("KUBER_API_URL", "https://preview.kuber.cardanoapi.io")
+KUBER_API_URL = f'https://{os.getenv("NETWORK","preview")}.kuber.cardanoapi.io'
 KUBER_API_KEY = os.getenv("KUBER_API_KEY")
+FAUCET_API_URL = f'https://faucet.${os.getenv("NETWORK","preview")}.world.dev.cardano.org'
+FACUET_API_KEY = os.getenv("FAUCET_API_KEY")

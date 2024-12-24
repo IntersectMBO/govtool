29f24b963418db6f5a13a73ad3c9de49b9515157
Michał Szałowski <michal.szalowski@gmail.com>
2024-12-24 16:12:18 +0100

    fix(#2460): fix calculating live voting power

--- govtool/backend/sql/get-voting-power.sql
+++ govtool/backend/sql/get-voting-power.sql
@@ -1,6 +1,21 @@
-select coalesce(sum(uv.value), 0) as amount
-from utxo_view uv
-join delegation_vote dv on uv.stake_address_id = dv.addr_id
-join drep_hash dh on dv.drep_hash_id = dh.id
-where dh.raw = decode(?,'hex')
-and dv.cert_index != 0
+WITH LatestDelegationVote AS (
+  SELECT 
+    addr_id,
+    MAX(id) AS latest_vote_id
+  FROM 
+    delegation_vote
+  GROUP BY 
+    addr_id
+)
+SELECT 
+  SUM(uv.value) AS total_value
+FROM 
+  utxo_view uv
+JOIN 
+  stake_address sa ON sa.id = uv.stake_address_id
+JOIN 
+  LatestDelegationVote ldv ON uv.stake_address_id = ldv.addr_id
+JOIN 
+  delegation_vote dv ON dv.id = ldv.latest_vote_id
+WHERE 
+  dv.drep_hash_id = (SELECT id FROM drep_hash WHERE raw = decode(?,'hex'))

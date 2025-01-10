9f630f146e03266d6125f268f41af2f79edb1804
Michał Szałowski <michal.szalowski@gmail.com>
2025-01-10 14:20:11 +0100

    chore: bump GovTool to v2.0.5

--- govtool/metadata-validation/src/main.ts
+++ govtool/metadata-validation/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Metadata Validation Tool')
     .setDescription('The Metadata Validation Tool API description')
-    .setVersion('2.0.4')
+    .setVersion('2.0.5')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);

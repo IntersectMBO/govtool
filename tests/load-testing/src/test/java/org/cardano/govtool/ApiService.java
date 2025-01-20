b2a45e5b1f7f10d3e77c82b22bc46d36c23d0a6b
Niraj <duwalkneerose@gmail.com>
2025-01-20 23:10:22 +0545

    feat: add API service methods for pdf api

--- tests/load-testing/src/test/java/org/cardano/govtool/ApiService.java
+++ tests/load-testing/src/test/java/org/cardano/govtool/ApiService.java
@@ -33,4 +33,13 @@ public class ApiService {
     public static ChainBuilder pollTxStatus = repeat(session -> ThreadLocalRandom.current()
             .nextInt(3,6)).on(
                 exec(http("get_tx_status").get("/transaction/status/#{txId}")).pause(4));
+
+    public static ChainBuilder getComments = exec(http("get_comments").get("/comments").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}"));
+    public static ChainBuilder postComments = exec(http("post_comments").post("/comments").body(StringBody("{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"comment_text\":\"#{commentText}\"}}")).header("content-type","application/json"));
+    public static ChainBuilder getActivePolls = exec(http("get_Active_polls").get("/polls").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}").queryParam("filters[$and][1][is_poll_active]", "true"));
+    public static ChainBuilder getInActivePolls = exec(http("get_Inactive_polls").get("/polls").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}").queryParam("filters[$and][1][is_poll_active]", "false"));
+    public static ChainBuilder proposalVote = exec(http("proposal_vote").post("/proposal-votes").body(StringBody("{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"vote_result\":#{proposalVote}}}")).header("content-type","application/json"));
+    public static ChainBuilder getGovernanceActionType = exec(http("get_gov_action_type").get("/governance-action-types"));
+    public static ChainBuilder createProposalDraft = exec(http("create_proposal_draft").post("/proposals").body(StringBody("{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":null,\"prop_amount\":null}],\"gov_action_type_id\":#{governanceActionTypeId},\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":true}}")).header("content-type","application/json"));
+    public static ChainBuilder createProposal = exec(http("create_proposal").post("/proposals").body(StringBody("{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":null,\"prop_amount\":null}],\"gov_action_type_id\":1,\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":false}}")).header("content-type","application/json"));
 }

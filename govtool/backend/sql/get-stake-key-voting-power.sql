568d6e52905882521b686784b6c8b7bdb06e41e8
Michał Szałowski <michal.szalowski@gmail.com>
2025-01-21 20:44:41 +0100

    fix: add missing select on stake key addr id

--- govtool/backend/sql/get-stake-key-voting-power.sql
+++ govtool/backend/sql/get-stake-key-voting-power.sql
@@ -1,5 +1,6 @@
 SELECT
-  SUM(tx_out.value)
+  COALESCE(SUM(tx_out.value), 0) AS voting_power,
+  stake_address.id as addr_id
 FROM
   stake_address
 JOIN
@@ -7,4 +8,6 @@ JOIN
 LEFT JOIN
   tx_in ON tx_in.tx_out_id = tx_out.id AND tx_in.tx_out_index = tx_out.index
 WHERE
-  stake_address.hash_raw = decode(?, 'hex') AND tx_in.id IS NULL;
+  stake_address.hash_raw = decode(?, 'hex') AND tx_in.id IS NULL
+GROUP BY
+  stake_address.id;

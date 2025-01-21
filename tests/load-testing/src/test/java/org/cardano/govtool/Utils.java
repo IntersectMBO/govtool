eee80fccc6c323c48a588a935930876c43ab162a
Niraj <duwalkneerose@gmail.com>
2025-01-21 20:15:55 +0545

    feat: add utils to extract jwt and bech32 stake address from auth json

--- tests/load-testing/src/test/java/org/cardano/govtool/Utils.java
+++ tests/load-testing/src/test/java/org/cardano/govtool/Utils.java
@@ -1,8 +1,15 @@
 package org.cardano.govtool;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bouncycastle.crypto.digests.Blake2bDigest;
 import org.bouncycastle.util.encoders.Hex;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 
 public class Utils {
@@ -40,23 +47,59 @@ public class Utils {
         return Hex.toHexString(stakeKey);
     }
 
-    public static String generateRandomDescription(){
+    public static String generateRandomDescription() {
         return faker.lorem().sentence();
     }
 
-    public static String generateRandomTitle(){
+    public static String generateRandomTitle() {
         return faker.name().title();
     }
 
-    public static String generateRandomLink(){
+    public static String generateRandomLink() {
         return faker.internet().url();
     }
 
-    public static int generateRandomAmount(){
-        return faker.number().randomDigitNotZero();
+    public static String generateRandomAmount() {
+        return String.valueOf(faker.number().numberBetween(100, 10000));
     }
 
-    public static boolean generateRandomBool(){
+    public static boolean generateRandomBool() {
         return faker.bool().bool();
+    }
+
+    public static String extractJWT() {
+        try {
+            String json = Files.readString(Paths.get("src/test/resources/auth.json"));
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(json);
+            var randomIndex = ThreadLocalRandom.current().nextInt(0, rootNode.size());
+            String jwt = rootNode.get(randomIndex).get("jwt").asText();
+            return jwt;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String extractBech32StakeAddress() {
+        try {
+            String json = Files.readString(Paths.get("src/test/resources/auth.json"));
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(json);
+            var randomIndex = ThreadLocalRandom.current().nextInt(0, rootNode.size());
+            String address = rootNode.get(randomIndex).get("stakeAddress").asText();
+            return address;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Map<String, String> proposalDefaultHeaders(String jwt) {
+        return Map.of(
+                "Authorization", "Bearer " + jwt,
+                "Content-Type", "application/json");
     }
 }

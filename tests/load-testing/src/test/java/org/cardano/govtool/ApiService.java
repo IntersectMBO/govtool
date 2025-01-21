9f73123c83a2b26e33e154617febf23bb1edbe6f
Niraj <duwalkneerose@gmail.com>
2025-01-21 20:17:21 +0545

    feat: update header and fix metadata validation test

--- tests/load-testing/src/test/java/org/cardano/govtool/ApiService.java
+++ tests/load-testing/src/test/java/org/cardano/govtool/ApiService.java
@@ -1,9 +1,9 @@
 package org.cardano.govtool;
 
-
 import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.http.HttpRequestActionBuilder;
 
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
@@ -11,15 +11,21 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class ApiService {
-    static String example_metadata_validate="{\"url\":\"https://metadata-govtool.cardanoapi.io/data/milanfile\",\"hash\":\"dcc25ca74534a1fa3b4c86447c1e28dd86e103e1ead5e5308b64b34338af00a9\"}";
-    public static ChainBuilder validate_sample_metadata= exec(http("validate_metadata").post("/metadata/validate").body(StringBody(example_metadata_validate)).header("content-type","application/json"));
-    public static ChainBuilder getCurrentDelegation = exec(http("get_current_delegation").get("/ada-holder/get-current-delegation/#{stakeKey}"));
+    private static final String METADATA_API_URL = Optional.ofNullable(System.getenv("METADATA_API_URL"))
+                        .orElse("https://z0656605b-zf21c9655-gtw.z937eb260.rustrocks.fr");
+    static String example_metadata_validate = "{\"url\":\"https://metadata-govtool.cardanoapi.io/data/milanfile\",\"hash\":\"dcc25ca74534a1fa3b4c86447c1e28dd86e103e1ead5e5308b64b34338af00a9\"}";
+    public static ChainBuilder validate_sample_metadata = exec(http("validate_metadata").post(METADATA_API_URL + "/validate")
+            .body(StringBody(example_metadata_validate)).header("content-type", "application/json"));
+    public static ChainBuilder getCurrentDelegation = exec(
+            http("get_current_delegation").get("/ada-holder/get-current-delegation/#{stakeKey}"));
 
-    public static HttpRequestActionBuilder getAdaHolderVotingPower = http("get_AdaHolder_voting_power").get("/ada-holder/get-voting-power/#{stakeKey}");
+    public static HttpRequestActionBuilder getAdaHolderVotingPower = http("get_AdaHolder_voting_power")
+            .get("/ada-holder/get-voting-power/#{stakeKey}");
 
-    public static HttpRequestActionBuilder getDRepVotingPower = http("get_dRep_voting_power").get("/drep/get-voting-power/#{dRepId}");
+    public static HttpRequestActionBuilder getDRepVotingPower = http("get_dRep_voting_power")
+            .get("/drep/get-voting-power/#{dRepId}");
 
-    public static  HttpRequestActionBuilder getDrepInfo = http("get_dRep_info").get("/drep/info/#{stakeKey}");
+    public static HttpRequestActionBuilder getDrepInfo = http("get_dRep_info").get("/drep/info/#{stakeKey}");
     public static ChainBuilder getVotes = exec(http("get_votes").get("/drep/getVotes/#{dRepId}"));
 
     public static ChainBuilder getAllDReps = exec(http("get_dReps").get("/drep/list?page=0&size=10"));
@@ -31,15 +37,40 @@ public class ApiService {
     public static ChainBuilder getMetrics = exec(http("get_metrics").get("/network/metrics"));
 
     public static ChainBuilder pollTxStatus = repeat(session -> ThreadLocalRandom.current()
-            .nextInt(3,6)).on(
-                exec(http("get_tx_status").get("/transaction/status/#{txId}")).pause(4));
+            .nextInt(3, 6)).on(
+                    exec(http("get_tx_status").get("/transaction/status/#{txId}")).pause(4));
 
-    public static ChainBuilder getComments = exec(http("get_comments").get("/comments").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}"));
-    public static ChainBuilder postComments = exec(http("post_comments").post("/comments").body(StringBody("{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"comment_text\":\"#{commentText}\"}}")).header("content-type","application/json"));
-    public static ChainBuilder getActivePolls = exec(http("get_Active_polls").get("/polls").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}").queryParam("filters[$and][1][is_poll_active]", "true"));
-    public static ChainBuilder getInActivePolls = exec(http("get_Inactive_polls").get("/polls").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}").queryParam("filters[$and][1][is_poll_active]", "false"));
-    public static ChainBuilder proposalVote = exec(http("proposal_vote").post("/proposal-votes").body(StringBody("{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"vote_result\":#{proposalVote}}}")).header("content-type","application/json"));
-    public static ChainBuilder getGovernanceActionType = exec(http("get_gov_action_type").get("/governance-action-types"));
-    public static ChainBuilder createProposalDraft = exec(http("create_proposal_draft").post("/proposals").body(StringBody("{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":null,\"prop_amount\":null}],\"gov_action_type_id\":#{governanceActionTypeId},\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":true}}")).header("content-type","application/json"));
-    public static ChainBuilder createProposal = exec(http("create_proposal").post("/proposals").body(StringBody("{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":null,\"prop_amount\":null}],\"gov_action_type_id\":1,\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":false}}")).header("content-type","application/json"));
+    public static ChainBuilder getComments = exec(
+            http("get_comments").get("/comments").queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}")
+                    .headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder postComments = exec(http("post_comments").post("/comments")
+            .body(StringBody(
+                    "{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"comment_text\":\"#{commentText}\"}}"))
+            .headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder getActivePolls = exec(http("get_Active_polls").get("/polls")
+            .queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}")
+            .queryParam("filters[$and][1][is_poll_active]", "true").headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder getInActivePolls = exec(http("get_Inactive_polls").get("/polls")
+            .queryParam("filters[$and][0][proposal_id]", "#{proposalDiscussionId}")
+            .queryParam("filters[$and][1][is_poll_active]", "false").headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder proposalVote = exec(http("proposal_vote").post("/proposal-votes")
+            .body(StringBody(
+                    "{\"data\":{\"proposal_id\":\"#{proposalDiscussionId}\",\"vote_result\":#{proposalVote}}}"))
+            .headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder getGovernanceActionType = exec(http("get_gov_action_type")
+            .get("/governance-action-types").headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder createProposalDraft = exec(http("create_proposal_draft").post("/proposals")
+            .body(StringBody(
+                    "{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":\"#{stakeAddress}\",\"prop_amount\":\"#{proposalAmount}\"}],\"gov_action_type_id\":#{governanceActionTypeId},\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":true}}"))
+            .headers(Utils.proposalDefaultHeaders("#{jwt}")));
+
+    public static ChainBuilder createProposal = exec(http("create_proposal").post("/proposals").body(StringBody(
+            "{\"data\":{\"proposal_links\":[{\"prop_link\":\"#{proposalLink}\",\"prop_link_text\":\"#{proposalLinkText}\"}],\"proposal_withdrawals\":[{\"prop_receiving_address\":\"#{stakeAddress}\",\"prop_amount\":\"#{proposalAmount}\"}],\"gov_action_type_id\":#{governanceActionTypeId},\"prop_name\":\"#{title}\",\"prop_abstract\":\"#{abstract}\",\"prop_motivation\":\"#{motivation}\",\"prop_rationale\":\"#{rationale}\",\"is_draft\":false}}"))
+            .headers(Utils.proposalDefaultHeaders("#{jwt}")));
 }
--- tests/load-testing/src/test/java/org/cardano/govtool/feeders/PageVisits.java
+++ tests/load-testing/src/test/java/org/cardano/govtool/feeders/PageVisits.java
@@ -9,8 +9,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
+
+import org.cardano.govtool.Utils;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
@@ -24,76 +27,72 @@ public class PageVisits {
             "HardForkInitiation",
             "ParameterChange",
             "TreasuryWithdrawals",
-            "InfoAction"
-    );
-    public static List<String> proposalCreationType =  Arrays.asList("InfoAction","TreasuryWithdrawals");
-    public static ObjectMapper objectMapper=new ObjectMapper();
-    public static HttpRequestActionBuilder listAndSelectDreps(int min, int max){
+            "InfoAction");
+    public static List<String> proposalCreationType = Arrays.asList("InfoAction", "TreasuryWithdrawals");
+    public static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static HttpRequestActionBuilder listAndSelectDreps(int min, int max) {
         return http("get_dReps").get("/drep/list?page=0&size=10")
-                .check(jmesPath("elements[].drepId").ofList().transform(dreps ->{
+                .check(jmesPath("elements[].drepId").ofList().transform(dreps -> {
                     Collections.shuffle(dreps);
                     int visited_dreps = ThreadLocalRandom.current().nextInt(min, max);
                     return dreps.subList(0, Math.min(visited_dreps, dreps.size()));
                 }).exists().saveAs("drepIds"));
     }
 
+    public static ChainBuilder visitProposalPage() {
+        final String METADATA_API_URL = Optional.ofNullable(System.getenv("METADATA_API_URL"))
+                .orElse("https://z0656605b-zf21c9655-gtw.z937eb260.rustrocks.fr");
 
-    public static ChainBuilder visitProposalPage(){
-
-
-        return exec(proposalTypes.stream().map(pType ->exec(requestProposal(pType))
-                .doIf(session -> session.get("metadataList"+pType)!=null)
-                .then(foreach("#{metadataList" +pType + "}","metadata"+pType)
-                .on(exec(
-                        http("validate proposal metadata")
-                                .post("/metadata/validate")
-                                .body(StringBody(session1 -> session1.get("metadata"+pType)
-                                )).header("content-type","application/json")
-                                .check(status().is(200))
-                )))
+        return exec(proposalTypes.stream().map(pType -> exec(requestProposal(pType))
+                .doIf(session -> session.get("metadataList" + pType) != null)
+                .then(foreach("#{metadataList" + pType + "}", "metadata" + pType)
+                        .on(exec(
+                                http("validate proposal metadata")
+                                        .post(METADATA_API_URL + "/validate")
+                                        .body(StringBody(session1 -> session1.get("metadata" + pType)))
+                                        .header("content-type", "application/json")
+                                        .check(status().is(201)))))
 
         ).toList());
     }
 
-    public static ChainBuilder visitProposalDiscussionPage(){
-        return exec(proposalCreationType.stream().map(pType ->exec(requestProposalDiscussion(pType))
-        ).toList());   
+    public static ChainBuilder visitProposalDiscussionPage() {
+        return exec(proposalCreationType.stream().map(pType -> exec(requestProposalDiscussion(pType))).toList());
     }
 
-   
-
-    public static HttpRequestActionBuilder requestProposalDiscussion(String type){
-        return http("list proposal Discussion type "+ type)
+    public static HttpRequestActionBuilder requestProposalDiscussion(String type) {
+        return http("list proposal Discussion type " + type)
                 .get("/proposals")
-                .queryParam("filters[$and][0][gov_action_type_id]",proposalCreationType.indexOf(type) + 1)
+                .header("authorization", "Bearer " + Utils.extractJWT())
+                .queryParam("filters[$and][0][gov_action_type_id]", proposalCreationType.indexOf(type) + 1)
                 .check(status().is(200));
     }
 
-    
-
-    public static HttpRequestActionBuilder requestProposal(String type){
-        return http("list proposal type "+type)
+    public static HttpRequestActionBuilder requestProposal(String type) {
+        return http("list proposal type " + type)
                 .get("/proposal/list")
-                .queryParam("page","0")
-                .queryParam("size","7")
-                .multivaluedQueryParam("type",List.of(type))
+                .queryParam("page", "0")
+                .queryParam("size", "7")
+                .multivaluedQueryParam("type", List.of(type))
                 .check(status().is(200))
                 .check(jmesPath("elements[*]").ofList().transform(rawElements -> {
 
-                    List<Map<String, Object>> elements = rawElements.stream().map(v -> (Map<String,Object>) v).toList();
+                    List<Map<String, Object>> elements = rawElements.stream().map(v -> (Map<String, Object>) v)
+                            .toList();
 
                     List<String> metaDataList = elements.stream().map(proposal -> {
-                                var metadataHash = proposal.get("metadataHash").toString();
-                                var url = proposal.get("url");
-                                var mp= Map.of("url",url,"hash",metadataHash);
-                                try {
-                                    return objectMapper.writeValueAsString(mp);
-                                } catch (JsonProcessingException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            })
+                        var metadataHash = proposal.get("metadataHash").toString();
+                        var url = proposal.get("url");
+                        var mp = Map.of("url", url, "hash", metadataHash);
+                        try {
+                            return objectMapper.writeValueAsString(mp);
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
                             .collect(Collectors.toList());
                     return metaDataList;
-                }).exists().saveAs("metadataList"+type));
+                }).exists().saveAs("metadataList" + type));
     }
 }

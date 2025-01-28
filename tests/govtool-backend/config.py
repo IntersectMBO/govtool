db27e12877319c6452003f25288cbed91385c1d6
Niraj <duwalkneerose@gmail.com>
2025-01-28 13:32:17 +0545

    chore: add dev api options on backend test deployment

--- tests/govtool-backend/config.py
+++ tests/govtool-backend/config.py
@@ -13,5 +13,5 @@ RECORD_METRICS_API = os.getenv("RECORD_METRICS_API")
 METRICS_API_SECRET = os.getenv("METRICS_API_SECRET")
 KUBER_API_URL = f'https://{os.getenv("NETWORK","preview")}.kuber.cardanoapi.io'
 KUBER_API_KEY = os.getenv("KUBER_API_KEY")
-FAUCET_API_URL = f'https://faucet.${os.getenv("NETWORK","preview")}.world.dev.cardano.org'
+FAUCET_API_URL = f'https://faucet.{os.getenv("NETWORK","preview")}.world.dev.cardano.org'
 FACUET_API_KEY = os.getenv("FAUCET_API_KEY")

464111c3ae3e03ef3d62e8d9e526b9a0356cbe52
Niraj <duwalkneerose@gmail.com>
2025-01-30 15:52:26 +0545

    feat: register Sentry only if sentryEnv is not 'qa'

--- govtool/backend/app/Main.hs
+++ govtool/backend/app/Main.hs
@@ -172,7 +172,7 @@ exceptionHandler vvaConfig sentryService mRequest exception = do
   let env = sentryEnv vvaConfig
   case mRequest of
     Nothing -> return ()
-    Just _  -> register
+    Just _  -> when (env /= "qa") $ register
       sentryService
       "vva.be"
       Error

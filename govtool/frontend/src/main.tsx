4c31d3effc7651d16caafe5f5cd71c799c9ea155
Niraj <duwalkneerose@gmail.com>
2025-01-30 14:08:14 +0545

    chore: conditionally initialize Sentry based on DSN availability

--- govtool/frontend/src/main.tsx
+++ govtool/frontend/src/main.tsx
@@ -24,28 +24,30 @@ const tagManagerArgs = {
 
 TagManager.initialize(tagManagerArgs);
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: import.meta.env.VITE_APP_ENV,
-  release: version,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  beforeSend(event) {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: "sentryEvent",
-      sentryEventId: event?.event_id || "default_event_id",
-      sentryErrorMessage:
-        event?.exception?.values?.[0]?.value || "Unknown Error",
-    });
-    return event;
-  },
-});
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.VITE_APP_ENV,
+    release: version,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    beforeSend(event) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "sentryEvent",
+        sentryEventId: event?.event_id || "default_event_id",
+        sentryErrorMessage:
+          event?.exception?.values?.[0]?.value || "Unknown Error",
+      });
+      return event;
+    },
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

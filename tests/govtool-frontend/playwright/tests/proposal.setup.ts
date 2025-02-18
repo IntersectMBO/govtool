9cfdfdab9c2a85c8ff1f4dc06561ad6d1b41f21a
Niraj <duwalkneerose@gmail.com>
2025-02-18 19:17:58 +0545

    feat: add proposal submission wallet setup

---
+++ tests/govtool-frontend/playwright/tests/proposal.setup.ts
@@ -0,0 +1,44 @@
+import environments from "@constants/environments";
+import { proposalFaucetWallet } from "@constants/proposalFaucetWallet";
+import { generateWallets } from "@helpers/shellyWallet";
+import { pollTransaction } from "@helpers/transaction";
+import { test as setup } from "@playwright/test";
+import kuberService from "@services/kuberService";
+import walletManager from "lib/walletManager";
+
+const PROPOSAL_WALLETS_COUNT = 4;
+
+let govActionDeposit: number;
+
+setup.beforeAll(async () => {
+  const res = await kuberService.queryProtocolParams();
+  govActionDeposit = res.govActionDeposit;
+});
+
+setup("Setup temporary proposal wallets", async () => {
+  setup.setTimeout(2 * environments.txTimeOut);
+
+  const proposalWallets = await generateWallets(PROPOSAL_WALLETS_COUNT);
+
+  // initialize wallets
+  const initializeRes = await kuberService.initializeWallets(
+    [...proposalWallets],
+    proposalFaucetWallet.address,
+    proposalFaucetWallet.payment.private
+  );
+  await pollTransaction(initializeRes.txId, initializeRes.lockInfo);
+
+  const amountOutputs = proposalWallets.map((wallet) => {
+    return { address: wallet.address, value: govActionDeposit };
+  });
+  const transferRes = await kuberService.multipleTransferADA(
+    amountOutputs,
+    proposalFaucetWallet.address,
+    proposalFaucetWallet.payment.private
+  );
+  await pollTransaction(transferRes.txId, transferRes.lockInfo);
+
+  // save to file
+  await walletManager.writeWallets(proposalWallets, "proposalSubmission");
+  await walletManager.writeWallets(proposalWallets, "proposalSubmissionCopy");
+});

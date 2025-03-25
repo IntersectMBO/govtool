97c355c754a7a64b6d7dd75e218a577707fe2738
Michał Szałowski <michal.szalowski@gmail.com>
2025-03-25 14:09:44 +0100

    chore: add ipfs debug logs

--- govtool/frontend/src/utils/openInNewTab.ts
+++ govtool/frontend/src/utils/openInNewTab.ts
@@ -7,6 +7,9 @@ export const openInNewTab = (url: string) => {
       ? `${import.meta.env.VITE_IPFS_GATEWAY}/${url.slice(7)}`
       : `https://${url}`;
 
+  // Open the URL in a new tab
+  console.debug("Opening in new tab", fullUrl);
+
   const newWindow = window.open(fullUrl, "_blank", "noopener,noreferrer");
   if (newWindow) newWindow.opener = null;
 };

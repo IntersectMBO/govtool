24aac2f39769df275dae6c26b862a88eba5c2db7
Niraj <duwalkneerose@gmail.com>
2025-04-02 13:29:54 +0545

    tests: restrict budget proposal creation in disconnect state

--- tests/govtool-frontend/playwright/tests/12-proposal-budget-submission/proposalBudgetSubmission.spec.ts
+++ tests/govtool-frontend/playwright/tests/12-proposal-budget-submission/proposalBudgetSubmission.spec.ts
@@ -1,12 +1,22 @@
 import { test } from "@fixtures/walletExtension";
 import { setAllureEpic } from "@helpers/allure";
-import { skipTestForProposalBudget } from "@helpers/cardano";
+import { isMobile } from "@helpers/mobile";
+import { expect } from "@playwright/test";
 
 test.beforeEach(async ({}) => {
   await setAllureEpic("12. Proposal Budget Submission");
-  await skipTestForProposalBudget();
 });
 
 test("12A. Should restrict from creating a budget proposal in disconnect state", async ({
   page,
-}) => {});
+}) => {
+  await page.goto("/");
+  if (isMobile(page)) {
+    await page.getByTestId("open-drawer-button").click();
+  }
+  await page.getByTestId("budget-discussion-link").click();
+  await expect(page.getByTestId("verify-identity-button")).not.toBeVisible();
+  await expect(
+    page.getByTestId("propose-a-budget-discussion-button")
+  ).not.toBeVisible();
+});

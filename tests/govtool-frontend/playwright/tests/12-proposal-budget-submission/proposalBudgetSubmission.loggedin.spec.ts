ce88512ff0cda2c843c227fba8f9352451468eaf
Niraj <duwalkneerose@gmail.com>
2025-04-04 13:00:53 +0545

    chore: update proposal creation page access test

--- tests/govtool-frontend/playwright/tests/12-proposal-budget-submission/proposalBudgetSubmission.loggedin.spec.ts
+++ tests/govtool-frontend/playwright/tests/12-proposal-budget-submission/proposalBudgetSubmission.loggedin.spec.ts
@@ -28,43 +28,50 @@ test.describe("Budget proposal 01 wallet", () => {
   test("12B. Should access proposal creation page in connected state", async ({
     page,
   }) => {
-    const budgetProposalSubmissionPage = new BudgetDiscussionSubmissionPage(
-      page
-    );
-    await budgetProposalSubmissionPage.goto();
+    await page.goto("/");
+    await page.getByTestId("budget-discussion-link").click();
+    await page.getByTestId("verify-identity-button").click();
+
     await expect(
-      budgetProposalSubmissionPage.beneficiaryFullNameInput
-    ).toBeVisible();
+      page.getByTestId("propose-a-budget-discussion-button")
+    ).toBeVisible({ timeout: 60_000 });
   });
 
-  test.describe("Budget proposal field verification", () => {
-    test("12D_1. Should verify all field of “contact information” section", async ({}) => {});
-    test("12D_2. Should verify all field of “proposal ownership” section", async ({}) => {});
-    test("12D_3. Should verify all field of “problem statements and proposal benefits” section", async ({}) => {});
-    test("12D_4. Should verify all field of “costing” section", async ({}) => {});
-    test("12D_5. Should verify all field of “further information” section", async ({}) => {});
-    test("12D_6. Should verify all field of “administration and auditing” section", async ({}) => {});
-    test("12D_7. Should verify all field of “submit” section", async ({}) => {});
-  });
+  test.describe("Budget proposal with proposalSubmissionPageNavigation", () => {
+    let budgetProposalSubmissionPage: BudgetDiscussionSubmissionPage;
+    test.beforeEach(async ({ page }) => {
+      const budgetSubmissionPage = new BudgetDiscussionSubmissionPage(page);
+      await budgetSubmissionPage.goto();
+    });
+    test.describe("Budget proposal field verification", () => {
+      test("12D_1. Should verify all field of “contact information” section", async ({}) => {});
+      test("12D_2. Should verify all field of “proposal ownership” section", async ({}) => {});
+      test("12D_3. Should verify all field of “problem statements and proposal benefits” section", async ({}) => {});
+      test("12D_4. Should verify all field of “costing” section", async ({}) => {});
+      test("12D_5. Should verify all field of “further information” section", async ({}) => {});
+      test("12D_6. Should verify all field of “administration and auditing” section", async ({}) => {});
+      test("12D_7. Should verify all field of “submit” section", async ({}) => {});
+    });
 
-  test.describe("Budget proposal field validation", () => {
-    test("12E_1. Should accept valid data in “contact information” section", async ({}) => {});
-    test("12E_2. Should accept valid data in “proposal ownership” section", async ({}) => {});
-    test("12E_3. Should accept valid data in “problem statements and proposal benefits” section", async ({}) => {});
-    test("12E_4. Should accept valid data in “costing” section", async ({}) => {});
-    test("12E_5. Should accept valid data in “further information” section", async ({}) => {});
-    test("12E_6. Should accept valid data in “administration and auditing” section", async ({}) => {});
-    test("12E_7. Should accept valid data in “submit” section", async ({}) => {});
-    test("12F_1. Should reject invalid data in “contact information” section", async ({}) => {});
-    test("12F_2. Should reject invalid data in “proposal ownership” section", async ({}) => {});
-    test("12F_3. Should reject invalid data in “problem statements and proposal benefits” section", async ({}) => {});
-    test("12F_4. Should reject invalid data in “costing” section", async ({}) => {});
-    test("12F_5. Should reject invalid data in “further information” section", async ({}) => {});
-    test("12F_6. Should reject invalid data in “administration and auditing” section", async ({}) => {});
-    test("12F_7. Should reject invalid data in “submit” section", async ({}) => {});
-  });
+    test.describe("Budget proposal field validation", () => {
+      test("12E_1. Should accept valid data in “contact information” section", async ({}) => {});
+      test("12E_2. Should accept valid data in “proposal ownership” section", async ({}) => {});
+      test("12E_3. Should accept valid data in “problem statements and proposal benefits” section", async ({}) => {});
+      test("12E_4. Should accept valid data in “costing” section", async ({}) => {});
+      test("12E_5. Should accept valid data in “further information” section", async ({}) => {});
+      test("12E_6. Should accept valid data in “administration and auditing” section", async ({}) => {});
+      test("12E_7. Should accept valid data in “submit” section", async ({}) => {});
+      test("12F_1. Should reject invalid data in “contact information” section", async ({}) => {});
+      test("12F_2. Should reject invalid data in “proposal ownership” section", async ({}) => {});
+      test("12F_3. Should reject invalid data in “problem statements and proposal benefits” section", async ({}) => {});
+      test("12F_4. Should reject invalid data in “costing” section", async ({}) => {});
+      test("12F_5. Should reject invalid data in “further information” section", async ({}) => {});
+      test("12F_6. Should reject invalid data in “administration and auditing” section", async ({}) => {});
+      test("12F_7. Should reject invalid data in “submit” section", async ({}) => {});
+    });
 
-  test("12G. Should validate and review submitted budget proposal", async ({}) => {});
+    test("12G. Should validate and review submitted budget proposal", async ({}) => {});
+  });
 });
 
 test("12C. Should save and view draft proposal", async ({ browser }) => {

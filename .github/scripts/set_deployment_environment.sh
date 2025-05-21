bc96e674bc5d7f4022bf8559b7d4e4e557007755
Niraj <duwalkneerose@gmail.com>
2025-05-21 15:53:50 +0545

    chore: remove unnecessary condition for group name

--- .github/scripts/set_deployment_environment.sh
+++ .github/scripts/set_deployment_environment.sh
@@ -2,28 +2,14 @@
 
 DEPLOYMENT=${DEPLOYMENT:-"govtool.cardanoapi.io/api"}
 GROUP_NAME="qa"
-HOST_URL=$HOST_URL
-BASE_URL=$BASE_URL
 
-# Determine if running from frontend (HOST_URL is set) or backend (BASE_URL is set)
-if [[ "$HOST_URL" != null && -n "$HOST_URL" ]]; then
-    if [[ "$DEPLOYMENT" == "preview.gov.tools" ]]; then
-        GROUP_NAME="preview"
-    elif [[ "$DEPLOYMENT" == "gov.tools" ]]; then
-        GROUP_NAME="mainnet"
-    else
-        GROUP_NAME="qa"
-    fi
-elif [[ -n "$BASE_URL" ]]; then
-    if [[ "$DEPLOYMENT" == "be.preview.gov.tools" || "$DEPLOYMENT" == "z6b8d2f7a-zca4a4c45-gtw.z937eb260.rustrocks.fr" ]]; then
-        GROUP_NAME="preview"
-    elif [[ "$DEPLOYMENT" == "be.gov.tools" ]]; then
-        GROUP_NAME="mainnet"
-    else
-        GROUP_NAME="qa"
-    fi
+if [[ "$DEPLOYMENT" == "preview.gov.tools" || "$DEPLOYMENT" == "be.preview.gov.tools" || "$DEPLOYMENT" == "z6b8d2f7a-zca4a4c45-gtw.z937eb260.rustrocks.fr" ]]; then
+    GROUP_NAME="preview"
+elif [[ "$DEPLOYMENT" == "gov.tools" || "$DEPLOYMENT" == "be.gov.tools" ]]; then
+    GROUP_NAME="mainnet"
+elif [[ "$DEPLOYMENT" == "p80-z78acf3c2-zded6a792-gtw.z937eb260.rustrocks.fr" || "$DEPLOYMENT" == "z78acf3c2-z5575152b-gtw.z937eb260.rustrocks.fr" ]]; then
+    GROUP_NAME="dev"
 else
-    echo "Warning: Neither HOST_URL nor BASE_URL is properly set. Using default QA environment."
     GROUP_NAME="qa"
 fi
 

7481c0581092fff2419b6bb5c55ab37cc447b23f
joseph rana <josephranamgr7@gmail.com>
2025-07-28 15:59:57 +0545

    chore: update vote rationale pinned card styles and text

--- govtool/frontend/src/components/organisms/VoteContext/VoteContextGovTool.tsx
+++ govtool/frontend/src/components/organisms/VoteContext/VoteContextGovTool.tsx
@@ -4,7 +4,7 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { useMutation } from "react-query";
 
 import { Spacer } from "@atoms";
-import { useTranslation } from "@hooks";
+import { useScreenDimension, useTranslation } from "@hooks";
 import { VoteContextWrapper } from "@organisms";
 import { postIpfs } from "@services";
 import { downloadTextFile, openInNewTab } from "@utils";
@@ -15,6 +15,7 @@ import { LINKS } from "@/consts/links";
 import { ICONS } from "@/consts/icons";
 import { useSnackbar } from "@context";
 import { copyToClipboard } from "@utils";
+import { primaryBlue } from "@/consts";
 
 interface PostIpfsResponse {
   ipfsCid: string;
@@ -43,6 +44,8 @@ export const VoteContextGovTool = ({
   const [uploadInitiated, setUploadInitiated] = useState(false); // New state to track upload
   const { t } = useTranslation();
   const { addSuccessAlert } = useSnackbar();
+
+  const { isMobile } = useScreenDimension();
 
   const openLink = () => openInNewTab(LINKS.STORING_INFORMATION_OFFLINE);
 
@@ -74,8 +77,9 @@ export const VoteContextGovTool = ({
       onContinue={submitVoteContext}
       isContinueDisabled={!apiResponse}
       onCancel={onCancel}
+      showAllButtons={false}
     >
-      <Typography sx={{ textAlign: "center" }} variant="h4">
+      <Typography sx={{ textAlign: "center" , fontSize : "28px" , fontWeight: 500 }} variant="h4">
         {t("createGovernanceAction.rationalePinnedToIPFS")}
       </Typography>
       <Link
@@ -85,63 +89,53 @@ export const VoteContextGovTool = ({
           fontSize: 16,
           fontWeight: 500,
           fontFamily: "Poppins",
-          my: 4,
+          my: 2,
           textAlign: "center",
           textDecoration: "none",
         }}
       >
-        {t("createGovernanceAction.readFullGuide")}
-        <OpenInNewIcon
-          sx={{
-            color: "primary",
-            height: 17,
-            width: 17,
-            ml: 0.5,
-            verticalAlign: "middle",
-          }}
-        />
+        {t("createGovernanceAction.learnMore")}
       </Link>
-      <Typography sx={{ textAlign: "center" }} variant="h5">
-        {t("createGovernanceAction.recommendations")}
-      </Typography>
-      <Spacer y={4} />
       {isLoading ? (
         <Box sx={{ display: "flex", justifyContent: "center" }}>
           <CircularProgress size={24} />
         </Box>
       ) : apiResponse ? (
         <>
-          <Typography fontWeight={400} sx={{ textAlign: "center" }} variant="body1">
-            {t("createGovernanceAction.downloadAndStoreMetadataFile")}
+          <Typography fontWeight={400} sx={{ textAlign: "center" , mt: 2 }} variant="body1">
+            {t("createGovernanceAction.optionalDownloadAndStoreMetadataFile")}
           </Typography>
           <Button
             data-testid="metadata-download-button"
             onClick={handleDownload}
             size="large"
             startIcon={<img alt="download" src={ICONS.download} />}
-            sx={{ width: "fit-content", alignSelf: "center", my: 4 }}
+            sx={{ width: "fit-content", alignSelf: "center" , my:3 , height : "48px", px : 7 , fontWeight : 500 }}
             variant="outlined"
           >
             {t("govActions.voteContextFileName")}
           </Button>
-          <Typography sx={{ textAlign: "center" }} variant="body1">
-            {t("createGovernanceAction.rePinYourFile")}
+          <Typography sx={{ textAlign: "center" , my:1 }} variant="body1">
+            {t("createGovernanceAction.rePinYourFileToIPFS")}
           </Typography>
           <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 1 }}>
-            <Typography sx={{ textAlign: "center" }} variant="body1">
-              {apiResponse.ipfsCid ? `ipfs://${apiResponse.ipfsCid}` : "[URI]"}
-            </Typography>
-            {apiResponse.ipfsCid && (
-              <Link
-                onClick={() => {
-                  copyToClipboard(`ipfs://${apiResponse.ipfsCid}`);
-                  addSuccessAlert(t("alerts.copiedToClipboard"));
-                }}
-                sx={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+         <Typography sx={{ textAlign: "center" ,
+            "&:hover": {
+                  textDecoration: "underline",
+            }, }} variant="body1">
+            {apiResponse.ipfsCid ? (
+              <a
+                href={`https://ipfs.io/ipfs/${apiResponse.ipfsCid}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: primaryBlue.c500, cursor: "pointer" }}
               >
-                <img alt="copy" src={ICONS.copyBlueIcon} style={{ width: 16, height: 16 }} />
-              </Link>
+                <span style={{fontWeight : 500}}>IPFS URI:</span> {`https://ipfs.io/ipfs/${apiResponse.ipfsCid}`}
+              </a>
+            ) : (
+              "[URI]"
             )}
+          </Typography>
           </Box>
         </>
       ) : (
@@ -149,6 +143,22 @@ export const VoteContextGovTool = ({
           {t("createGovernanceAction.uploadingToIPFS")}
         </Typography>
       )}
+        <Box sx={{ display: "flex", flexDirection: isMobile ? "column" : "row" ,  width: "100%", justifyContent: isMobile ? "none" : "space-between" , gap: isMobile ? "14px" : "0px" , mt: 3 }}>
+                <Button
+                  variant="outlined"
+                  onClick={()=>{setStep(2)}}
+                  sx={{ width: isMobile ? "100%" : "96px", whiteSpace: "nowrap" , height:"48px" , fontWeight:"500" }}
+                >
+                  {t("createGovernanceAction.back")}
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={submitVoteContext}
+                  sx={{ width: isMobile ? "100%" :  "130px", whiteSpace: "nowrap" , height:"48px" , fontWeight:"500" }}
+                >
+                  {t("createGovernanceAction.continue")}
+                </Button>
+              </Box>
     </VoteContextWrapper>
   );
 };
